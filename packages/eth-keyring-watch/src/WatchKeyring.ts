ae734a35104213ee178695552259520963268528
richardo2016x <richardo2016x@gmail.com>
2023-12-07 19:42:49 +0800

    build: mono-style scaffold, support apps/* and packages/*

---
+++ packages/eth-keyring-watch/src/WatchKeyring.ts
@@ -0,0 +1,94 @@
+import { EventEmitter } from 'events';
+import { addHexPrefix, bufferToHex, isValidAddress } from 'ethereumjs-util';
+
+const keyringType: string = 'Watch Address';
+
+function sanitizeHex(hex: string): string {
+  hex = hex.substring(0, 2) === '0x' ? hex.substring(2) : hex;
+  if (hex === '') {
+    return '';
+  }
+  hex = hex.length % 2 !== 0 ? '0' + hex : hex;
+  return '0x' + hex;
+}
+
+class WatchKeyring extends EventEmitter {
+  static type: string = keyringType;
+  type: string = keyringType;
+  accounts: string[] = [];
+  accountToAdd: string = '';
+
+  constructor(opts: { accounts?: string[] } = {}) {
+    super();
+    this.deserialize(opts);
+  }
+
+  serialize(): Promise<{ accounts: string[] }> {
+    return Promise.resolve({
+      accounts: this.accounts,
+    });
+  }
+
+  async deserialize(opts?: { accounts?: string[] }): Promise<void> {
+    if (opts?.accounts) {
+      this.accounts = opts.accounts;
+    }
+  }
+
+  setAccountToAdd = (account: string): void => {
+    this.accountToAdd = account;
+  };
+
+  addAccounts = async (): Promise<string[]> => {
+    if (!isValidAddress(this.accountToAdd)) {
+      throw new Error("The address you're are trying to import is invalid");
+    }
+    const prefixedAddress: string = addHexPrefix(this.accountToAdd);
+
+    if (
+      this.accounts
+        .map((x) => x.toLowerCase())
+        .includes(prefixedAddress.toLowerCase())
+    ) {
+      throw new Error("The address you're are trying to import is duplicated");
+    }
+
+    this.accounts.push(prefixedAddress);
+
+    return [prefixedAddress];
+  };
+
+  async signTransaction(address: string, transaction: any): Promise<void> {
+    // TODO: split by protocol(walletconnect, cold wallet, etc)
+    throw new Error('Can not sign with watch address');
+  }
+
+  async signPersonalMessage(address: string, message: string): Promise<void> {
+    throw new Error('Can not sign with watch address');
+  }
+
+  async signTypedData(address: string, data: any): Promise<void> {
+    throw new Error('Can not sign with watch address');
+  }
+
+  async getAccounts(): Promise<string[]> {
+    return this.accounts.slice();
+  }
+
+  removeAccount(address: string): void {
+    if (
+      !this.accounts.map((a) => a.toLowerCase()).includes(address.toLowerCase())
+    ) {
+      throw new Error(`Address ${address} not found in watch keyring`);
+    }
+    this.accounts = this.accounts.filter(
+      (a) => a.toLowerCase() !== address.toLowerCase()
+    );
+  }
+
+  _normalize(buf: Buffer | string): string {
+    return sanitizeHex(typeof buf === 'string' ? buf : bufferToHex(buf).toString());
+  }
+}
+
+export default WatchKeyring;
