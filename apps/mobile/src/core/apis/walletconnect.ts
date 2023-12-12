2a28effa0fb7a306bbe76a0fc4cd453d31ba1487
qingwei-li <heisenberg-2077@outlook.com>
2023-12-12 11:26:40 +0800

    feat: add catch

--- apps/mobile/src/core/apis/walletconnect.ts
+++ apps/mobile/src/core/apis/walletconnect.ts
@@ -3,11 +3,18 @@ import { KEYRING_TYPE } from '@rabby-wallet/keyring-utils';
 import { getKeyring } from './keyring';
 
 export async function getUri(brandName: string) {
-  const keyring = (await getKeyring(
-    KEYRING_TYPE.WalletConnectKeyring,
-  )) as unknown as WalletConnectKeyring;
+  let uri: string | undefined;
 
-  const { uri } = await keyring.initConnector(brandName);
+  try {
+    const keyring = (await getKeyring(
+      KEYRING_TYPE.WalletConnectKeyring,
+    )) as unknown as WalletConnectKeyring;
+
+    const res = await keyring.initConnector(brandName);
+    uri = res.uri;
+  } catch (e) {
+    console.error(e);
+  }
 
   return uri;
 }
