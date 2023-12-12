f405960efcc9ef8ea3821b7a957f6fda9469ecc4
qingwei-li <heisenberg-2077@outlook.com>
2023-12-12 17:59:39 +0800

    feat: init import success

--- apps/mobile/src/screens/Address/ImportSuccessScreen.tsx
+++ apps/mobile/src/screens/Address/ImportSuccessScreen.tsx
@@ -1,5 +1,45 @@
 import { Text } from '@/components';
+import RootScreenContainer from '@/components/ScreenContainer/RootScreenContainer';
+import { ScreenColors } from '@/constant/layout';
+import { useThemeColors } from '@/hooks/theme';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { AddressInput } from './components/AddressInput';
 
 export const ImportSuccessScreen = () => {
-  return <Text>123</Text>;
+  const colors = useThemeColors();
+  const styles = React.useMemo(
+    () =>
+      StyleSheet.create({
+        rootContainer: {
+          display: 'flex',
+          backgroundColor: ScreenColors.homeHeaderBlue,
+        },
+        titleContainer: {
+          width: '100%',
+          height: 320,
+          flexShrink: 0,
+          backgroundColor: ScreenColors.homeHeaderBlue,
+        },
+        inputContainer: {
+          backgroundColor: colors['neutral-bg-2'],
+          height: '100%',
+          paddingVertical: 24,
+          paddingHorizontal: 20,
+        },
+      }),
+
+    [colors],
+  );
+
+  return (
+    <RootScreenContainer style={styles.rootContainer}>
+      <View style={styles.titleContainer}>
+        <Text>Added successfully</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <AddressInput />
+      </View>
+    </RootScreenContainer>
+  );
 };
---
+++ apps/mobile/src/screens/Address/components/AddressInput.tsx
@@ -0,0 +1,59 @@
+import { useThemeColors } from '@/hooks/theme';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import CopySVG from '@/assets/icons/common/copy.svg';
+import { Button } from '@/components';
+
+interface Props {
+  address: string;
+  aliasName: string;
+}
+
+export const AddressInput: React.FC<Props> = ({ address, aliasName }) => {
+  const colors = useThemeColors();
+  const styles = React.useMemo(
+    () =>
+      StyleSheet.create({
+        container: {
+          backgroundColor: colors['neutral-card-1'],
+          borderRadius: 8,
+          paddingHorizontal: 10,
+          paddingVertical: 12,
+        },
+        input: {
+          backgroundColor: colors['neutral-card-2'],
+          borderRadius: 4,
+          paddingHorizontal: 6,
+          paddingVertical: 11,
+          marginRight: 80,
+          marginBottom: 10,
+        },
+        address: {
+          color: colors['neutral-body'],
+          fontSize: 13,
+          fontWeight: '400',
+          marginRight: 4,
+        },
+      }),
+
+    [colors],
+  );
+
+  const onCopy = React.useCallback(() => {
+    // TODO: copy address
+    console.log('onCopy');
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <TextInput defaultValue={aliasName} style={styles.input} />
+      <View>
+        <Text style={styles.address}>{address}</Text>
+
+        <Button onPress={onCopy}>
+          <CopySVG />
+        </Button>
+      </View>
+    </View>
+  );
+};
