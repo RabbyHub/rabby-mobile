e6d7db541ddc4ff798d81b2c8c7d9fb3a4f3fd14
richardo2016x <richardo2016x@gmail.com>
2023-12-17 02:05:04 +0800

    build: support deploy ipa for pretest.

--- apps/mobile/scripts/mk_icons.sh
+++ apps/mobile/scripts/mk_icons.sh
@@ -6,31 +6,36 @@ project_dir=$(dirname "$script_dir")
 ios_godfile_dir=$script_dir/bundles;
 
 mk_ios_icons() {
+  local ios_icons_dir=$project_dir/ios/RabbyMobile/Images.xcassets/;
+
   # for iOS
   local ios_sizes=(
     ## AppIcon.appiconset
     # iPhone Notification
-    40 40 "2x" logo-1024w.png AppIcon.appiconset
-    60 60 "3x" logo-1024w.png AppIcon.appiconset
+    40 40 "2x" logo-1024w.png $ios_icons_dir/AppIcon.appiconset
+    60 60 "3x" logo-1024w.png $ios_icons_dir/AppIcon.appiconset
     # iPhone Settings
-    58 58 "2x" logo-1024w.png AppIcon.appiconset
-    87 87 "3x" logo-1024w.png AppIcon.appiconset
+    58 58 "2x" logo-1024w.png $ios_icons_dir/AppIcon.appiconset
+    87 87 "3x" logo-1024w.png $ios_icons_dir/AppIcon.appiconset
     # iPhone Spotlight
-    80 80 "2x" logo-1024w.png AppIcon.appiconset
-    120 120 "3x" logo-1024w.png AppIcon.appiconset
+    80 80 "2x" logo-1024w.png $ios_icons_dir/AppIcon.appiconset
+    120 120 "3x" logo-1024w.png $ios_icons_dir/AppIcon.appiconset
     # iPhone App
-    120 120 "2x" logo-1024w.png AppIcon.appiconset
-    180 180 "3x" logo-1024w.png AppIcon.appiconset
+    120 120 "2x" logo-1024w.png $ios_icons_dir/AppIcon.appiconset
+    180 180 "3x" logo-1024w.png $ios_icons_dir/AppIcon.appiconset
     # App Store
-    1024 1024 "1x" logo-1024w.png AppIcon.appiconset
+    1024 1024 "1x" logo-1024w.png $ios_icons_dir/AppIcon.appiconset
 
     ## LaunchScreen.imageset
-    91 79 "1x" splash-logo-512w.png LaunchScreen.imageset
-    182 158 "2x" splash-logo-512w.png LaunchScreen.imageset
-    273 237 "3x" splash-logo-512w.png LaunchScreen.imageset
+    91 79 "1x" splash-logo-512w.png $ios_icons_dir/LaunchScreen.imageset
+    182 158 "2x" splash-logo-512w.png $ios_icons_dir/LaunchScreen.imageset
+    273 237 "3x" splash-logo-512w.png $ios_icons_dir/LaunchScreen.imageset
+
+    ## For Deployment
+    512 512 "512w" logo-1024w.png $script_dir/deployments/ios/
+    57 57 "57w" logo-1024w.png $script_dir/deployments/ios/
   )
 
-  local ios_icons_dir=$project_dir/ios/RabbyMobile/Images.xcassets/;
 
   for ((i=0;i<${#ios_sizes[@]};i+=5))
   do
@@ -38,9 +43,9 @@ mk_ios_icons() {
       local h=${ios_sizes[i+1]}
       local scale=${ios_sizes[i+2]}
       local godfile=${ios_sizes[i+3]}
-      local setname=${ios_sizes[i+4]}
+      local targetdir=${ios_sizes[i+4]}
 
-      sips -z $h $w $ios_godfile_dir/$godfile --out $ios_icons_dir/$setname/icon_${w}x${h}@${scale}.png
+      sips -z $h $w $ios_godfile_dir/$godfile --out $targetdir/icon_${w}x${h}@${scale}.png
   done
 }
 
