0aa4390974d3ceaca6e83d5ec1cef26b1a002df7
richardo2016x <richardo2016x@gmail.com>
2023-12-20 16:45:36 +0800

    fix: rn EventEmitter.

--- packages/base-utils/src/index.ts
+++ packages/base-utils/src/index.ts
@@ -2,3 +2,5 @@ export * from './type-helpers';
 
 export * as addressUtils from './utils/address';
 export * as stringUtils from './utils/string';
+
+export { RNEventEmitter } from './utils/patch/event';
