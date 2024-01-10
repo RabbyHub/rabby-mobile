70b8d971d1c0759dd36fccb7d0af023a662a8cfa
richardo2016x <richardo2016x@gmail.com>
2024-01-10 18:25:41 +0800

    feat: allow send most test rpc methods from test-dapp.

--- packages/rn-webview-bridge/scripts/build-inpage-bridge.sh
+++ packages/rn-webview-bridge/scripts/build-inpage-bridge.sh
@@ -8,7 +8,18 @@ repo_dir=$(dirname $(dirname $project_dir))
 
 [[ -z "${webpack_mode}" ]] && webpack_mode="production";
 
-rm -f $repo_dir/apps/mobile/android/app/src/main/assets/InpageBridgeWeb3.js
+files=(
+  # for iOS
+  "$repo_dir/apps/mobile/src/core/bridges/InpageBridgeWeb3.js"
+  # for Android
+  "$repo_dir/apps/mobile/assets/custom/InpageBridgeWeb3.js"
+)
+# remove all files
+for file in "${files[@]}"
+do
+  rm -f $file
+done
+
 mkdir -p $script_dir/inpage-bridge/dist && rm -rf $script_dir/inpage-bridge/dist/*
 cd $script_dir/inpage-bridge/inpage
 $repo_dir/node_modules/.bin/webpack --config webpack.config.js --mode $webpack_mode
@@ -19,4 +30,11 @@ cat dist/inpage-bundle.js content-script/index.js > dist/index-raw.js
 $repo_dir/node_modules/.bin/webpack --config webpack.config.js --mode $webpack_mode
 
 # cp $script_dir/inpage-bridge/dist/index.js $repo_dir/apps/mobile/src/core/bridges/InpageBridgeWeb3.js
-cp $script_dir/inpage-bridge/dist/index.js $repo_dir/apps/mobile/android/app/src/main/assets/InpageBridgeWeb3.js
+# cp $script_dir/inpage-bridge/dist/index.js $repo_dir/apps/mobile/android/app/src/main/assets/InpageBridgeWeb3.js
+
+# copy dist to targets
+for file in "${files[@]}"
+do
+  mkdir -p $(dirname $file)
+  cp $script_dir/inpage-bridge/dist/index.js $file
+done
