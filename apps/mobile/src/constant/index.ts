692adde54d17307444214069cb9f813b1df8c62f
richardo2016x <104543757+richardo2016x@users.noreply.github.com>
2024-02-02 16:24:26 +0800

    build: fix. (#98)

--- apps/mobile/src/constant/index.ts
+++ apps/mobile/src/constant/index.ts
@@ -3,13 +3,20 @@ export const INITIAL_OPENAPI_URL = 'https://app-api.rabby.io';
 
 export const INITIAL_TESTNET_OPENAPI_URL = 'https://api.testnet.rabby.io';
 
-// TODO: 确定一个合适的值
-export const INTERNAL_REQUEST_ORIGIN = 'rabby-mobile';
+export const INTERNAL_REQUEST_ORIGIN =
+  'chrome-extension://acmacodkjbdgmoleebolmdjonilkdbch';
 
 export const INTERNAL_REQUEST_SESSION = {
   name: 'Rabby',
   origin: INTERNAL_REQUEST_ORIGIN,
   icon: './images/rabby-site-logo.png',
+};
+
+export const APP_VERSION = process.env.APP_VERSION;
+
+export const APP_URLS = {
+  PRIVACY_POLICY: 'https://rabby.io/docs/privacy',
+  TWITTER: 'https://twitter.com/rabby_io',
 };
 
 export enum CANCEL_TX_TYPE {
