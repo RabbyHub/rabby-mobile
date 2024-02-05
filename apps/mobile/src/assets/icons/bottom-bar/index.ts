930a8e13ad00390a6077605c26ed2dc02006872e
DMY <147dmy@gmail.com>
2024-02-05 18:29:57 +0800

    fix: reset (#111)
    * fix: bottom icons
    
    * fix: bottom icons
    
    * fix: cancel button bgcolor
    
    * fix: ios condition
    
    * fix: reset to importNewAddress after empty addr
    
    * fix: settings

--- apps/mobile/src/assets/icons/bottom-bar/index.ts
+++ apps/mobile/src/assets/icons/bottom-bar/index.ts
@@ -1,5 +1,8 @@
 import { default as RcIconNavigationHomeLightCC } from './nav-home-light-cc.svg';
+import { default as RcIconNavigationHomeFocusLightCC } from './nav-home-focus-light-cc.svg';
+
 import { default as RcIconNavigationDappsLightCC } from './nav-dapps-light-cc.svg';
+import { default as RcIconNavigationDappsFocusLightCC } from './nav-dapps-focus-light-cc.svg';
 
 import { makeActiveIconFromCC } from '@/hooks/makeThemeIcon';
 import { ThemeColors } from '@/constant/theme';
@@ -11,11 +14,26 @@ export const RcIconNavigationHomeLight = makeActiveIconFromCC(
     inactiveAcolor: ThemeColors.light['neutral-foot'],
   },
 );
+export const RcIconNavigationHomeFocusLight = makeActiveIconFromCC(
+  RcIconNavigationHomeFocusLightCC,
+  {
+    activeColor: ThemeColors.light['blue-default'],
+    inactiveAcolor: ThemeColors.light['blue-default'],
+  },
+);
 
 export const RcIconNavigationDappsLight = makeActiveIconFromCC(
   RcIconNavigationDappsLightCC,
   {
-    activeColor: ThemeColors.light['blue-default'],
+    activeColor: ThemeColors.light['neutral-foot'],
     inactiveAcolor: ThemeColors.light['neutral-foot'],
+  },
+);
+
+export const RcIconNavigationDappsFocusLight = makeActiveIconFromCC(
+  RcIconNavigationDappsFocusLightCC,
+  {
+    activeColor: ThemeColors.light['blue-default'],
+    inactiveAcolor: ThemeColors.light['blue-default'],
   },
 );
