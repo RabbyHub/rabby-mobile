5493c0160a3375d1a9aa1bb8834ece5962b84c7d
richardo2016x <richardo2016x@gmail.com>
2024-02-06 19:25:45 +0800

    build: update ios adhoc installation name.

--- apps/mobile/scripts/deploy-ios-adhoc.sh
+++ apps/mobile/scripts/deploy-ios-adhoc.sh
@@ -11,13 +11,15 @@ checkout_s3_pub_deployment_params;
 # make plist file
 cd $project_dir;
 proj_version=$(node --eval="process.stdout.write(require('./package.json').version)");
+app_display_name=$(node --eval="process.stdout.write(require('./app.json').displayName)");
 cd $script_dir;
 
 replace_variables $script_dir/tpl/ios/manifest.plist $script_dir/deployments/ios/manifest.plist \
   --var-IPA_DOWNLOAD_URL="$cdn_deployment_urlbase/ios/rabbymobile.ipa" \
   --var-URL_DISPLAY_IMAGE="$cdn_deployment_urlbase/ios/icon_57x57@57w.png" \
   --var-URL_FULL_SIZE_IMAGE="$cdn_deployment_urlbase/ios/icon_512x512@512w.png" \
-  --var-APP_VERSION="$proj_version"
+  --var-APP_VERSION="$proj_version" \
+  --var-APP_DISPLAY_NAME="$app_display_name"
 
 replace_variables $script_dir/tpl/ios/version.json $script_dir/deployments/ios/version.json \
   --var-DOWNLOAD_URL=$cdn_deployment_urlbase/ios/ \
