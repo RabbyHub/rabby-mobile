32965bc4875a5056b224a1ecf92bdbdd7b192fa3
richardo2016x <richardo2016x@gmail.com>
2024-02-07 13:59:26 +0800

    feat: support discard confirm modal on close select-address-sheet-modal.

--- apps/mobile/src/components/SelectAddress/SelectAddressSheetModal.tsx
+++ apps/mobile/src/components/SelectAddress/SelectAddressSheetModal.tsx
@@ -1,7 +1,12 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { Dimensions, View, Text } from 'react-native';
 import { useTranslation } from 'react-i18next';
-import { BottomSheetScrollView, BottomSheetView } from '@gorhom/bottom-sheet';
+import {
+  BottomSheetBackdrop,
+  BottomSheetBackdropProps,
+  BottomSheetScrollView,
+  BottomSheetView,
+} from '@gorhom/bottom-sheet';
 
 import { AppBottomSheetModal } from '../customized/BottomSheet';
 import { useSheetModal } from '@/hooks/useSheetModal';
@@ -23,17 +28,17 @@ import {
 } from '../GlobalBottomSheetModal';
 import FooterComponentForConfirm from '../customized/FooterComponentForConfirm';
 import { MODAL_NAMES } from '../GlobalBottomSheetModal/types';
+import ModalConfirmDiscard from './components/ModalConfirmDiscard';
+import AppBottomSheetBackdrop from '../patches/BottomSheetBackdrop';
 
 export interface SelectAddressProps {
   visible: boolean;
-  address?: string;
   onConfirm?(account: UIContactBookItem): void;
   onCancel?(): void;
 }
 
 export function SelectAddressSheetModal({
   visible,
-  address,
   onConfirm,
   onCancel,
 }: React.PropsWithoutRef<RNViewProps & SelectAddressProps>) {
@@ -49,7 +54,8 @@ export function SelectAddressSheetModal({
 
   const { accountsList, fetchAllAccountsToDisplay } = useAccountsToDisplay();
 
-  const { on: isEditing, turn: toggleEditing } = useSwitch();
+  const [isEditing, setEditing] = useState(false);
+  const [isConfirmingDiscard, setIsConfirmDiscard] = useState(false);
   const { whitelist, whitelistEnabled, setWhitelist } = useWhitelist();
   const [localWhiteList, setLocalWhiteList] = useState<string[]>([]);
   useEffect(() => {
@@ -82,7 +88,7 @@ export function SelectAddressSheetModal({
   }, [whitelistEnabled, accountsList, whitelist]);
 
   const confirmIdRef = useRef<string | null>(null);
-  const onPressButton = useCallback(async () => {
+  const onPressSaveButton = useCallback(async () => {
     if (isEditing) {
       if (confirmIdRef.current) return;
 
@@ -106,7 +112,7 @@ export function SelectAddressSheetModal({
                 onConfirm={async () => {
                   try {
                     await setWhitelist(localWhiteList);
-                    toggleEditing(!isEditing);
+                    setEditing(!isEditing);
                   } finally {
                     clearConfirm();
                   }
@@ -120,9 +126,41 @@ export function SelectAddressSheetModal({
         },
       });
     } else {
-      toggleEditing(!isEditing);
+      setEditing(!isEditing);
     }
-  }, [t, isEditing, toggleEditing, setWhitelist, localWhiteList]);
+  }, [t, isEditing, setEditing, setWhitelist, localWhiteList]);
+
+  const onModalDismiss = useCallback(() => {
+    if (isEditing) {
+      setIsConfirmDiscard(true);
+    } else {
+      onCancel?.();
+    }
+  }, [isEditing, onCancel]);
+  const onCancelDiscard = useCallback(() => {
+    setIsConfirmDiscard(false);
+  }, []);
+  const onConfirmedDiscard = useCallback(() => {
+    setIsConfirmDiscard(false);
+    setEditing(false);
+
+    onCancel?.();
+  }, [setEditing, onCancel]);
+
+  const renderBackdrop = useCallback(
+    (props: BottomSheetBackdropProps) => {
+      return (
+        <AppBottomSheetBackdrop
+          {...props}
+          pressBehavior="none"
+          onPress={onModalDismiss}
+          disappearsOnIndex={-1}
+          appearsOnIndex={0}
+        />
+      );
+    },
+    [onModalDismiss],
+  );
 
   const { onHardwareBackHandler } = useHandleBackPressClosable(
     useCallback(() => {
@@ -133,88 +171,99 @@ export function SelectAddressSheetModal({
   useFocusEffect(onHardwareBackHandler);
 
   return (
-    <AppBottomSheetModal
-      ref={sheetModalRef}
-      index={0}
-      snapPoints={['70%']}
-      onDismiss={onCancel}
-      footerComponent={() => (
-        <View style={[styles.footerContainer, styles.innerBlock]}>
-          <Button
-            containerStyle={styles.footerButtonContainer}
-            title={
-              isEditing
-                ? `Save to Whitelist (${localWhiteList.length})`
-                : 'Edit Whitelist'
-            }
-            onPress={onPressButton}
-          />
-        </View>
-      )}
-      enableContentPanningGesture={false}>
-      <BottomSheetView style={[styles.container]}>
-        <BottomSheetHandlableView style={[styles.titleArea, styles.innerBlock]}>
-          <Text style={[styles.modalTitle, styles.modalMainTitle]}>
-            {t('component.Contact.ListModal.title')}
-          </Text>
-          <View>
-            <Text style={[styles.modalTitle, styles.modalSubTitle]}>
-              {whitelistEnabled
-                ? t('component.Contact.ListModal.whitelistEnabled')
-                : t('component.Contact.ListModal.whitelistDisabled')}
+    <>
+      <AppBottomSheetModal
+        ref={sheetModalRef}
+        index={0}
+        snapPoints={['70%']}
+        enablePanDownToClose={!isEditing}
+        enableDismissOnClose={true}
+        backdropComponent={renderBackdrop}
+        onDismiss={onModalDismiss}
+        footerComponent={() => (
+          <View style={[styles.footerContainer, styles.innerBlock]}>
+            <Button
+              containerStyle={styles.footerButtonContainer}
+              title={
+                isEditing
+                  ? `Save to Whitelist (${localWhiteList.length})`
+                  : 'Edit Whitelist'
+              }
+              onPress={onPressSaveButton}
+            />
+          </View>
+        )}
+        enableContentPanningGesture={false}>
+        <BottomSheetView style={[styles.container]}>
+          <BottomSheetHandlableView
+            style={[styles.titleArea, styles.innerBlock]}>
+            <Text style={[styles.modalTitle, styles.modalMainTitle]}>
+              {t('component.Contact.ListModal.title')}
             </Text>
-          </View>
-        </BottomSheetHandlableView>
+            <View>
+              <Text style={[styles.modalTitle, styles.modalSubTitle]}>
+                {whitelistEnabled
+                  ? t('component.Contact.ListModal.whitelistEnabled')
+                  : t('component.Contact.ListModal.whitelistDisabled')}
+              </Text>
+            </View>
+          </BottomSheetHandlableView>
 
-        <BottomSheetScrollView style={[styles.scrollableBlock]}>
-          <View style={[styles.accountList, styles.innerBlock]}>
-            {sortedAccountsList.map((account, idx, arr) => {
-              const key = `${account.address}-${account.brandName}-${account.aliasName}`;
-              const inWhitelistPersisted = !!whitelist.find(wa =>
-                addressUtils.isSameAddress(wa, account.address),
-              );
-              const inWhitelistLocal = !isEditing
-                ? inWhitelistPersisted
-                : !!localWhiteList.find(wa =>
-                    addressUtils.isSameAddress(wa, account.address),
-                  );
+          <BottomSheetScrollView style={[styles.scrollableBlock]}>
+            <View style={[styles.accountList, styles.innerBlock]}>
+              {sortedAccountsList.map((account, idx, arr) => {
+                const key = `${account.address}-${account.brandName}-${account.aliasName}`;
+                const inWhitelistPersisted = !!whitelist.find(wa =>
+                  addressUtils.isSameAddress(wa, account.address),
+                );
+                const inWhitelistLocal = !isEditing
+                  ? inWhitelistPersisted
+                  : !!localWhiteList.find(wa =>
+                      addressUtils.isSameAddress(wa, account.address),
+                    );
 
-              return (
-                <AccountCard
-                  key={key}
-                  account={account}
-                  inWhitelist={inWhitelistLocal || !whitelistEnabled}
-                  isEditing={isEditing}
-                  style={[
-                    idx > 0 && { marginTop: 16 },
-                    idx === arr.length - 1 && { marginBottom: 16 },
-                  ]}
-                  onPress={() => {
-                    if (isEditing) {
-                      setLocalWhiteList(wl => {
-                        if (inWhitelistLocal) {
-                          return wl.filter(
-                            w =>
-                              !addressUtils.isSameAddress(w, account.address),
-                          );
-                        } else {
-                          return [...wl, account.address];
-                        }
-                      });
-                    } else {
-                      onConfirm?.({
-                        name: account.aliasName,
-                        address: account.address,
-                      });
-                    }
-                  }}
-                />
-              );
-            })}
-          </View>
-        </BottomSheetScrollView>
-      </BottomSheetView>
-    </AppBottomSheetModal>
+                return (
+                  <AccountCard
+                    key={key}
+                    account={account}
+                    inWhitelist={inWhitelistLocal || !whitelistEnabled}
+                    isEditing={isEditing}
+                    style={[
+                      idx > 0 && { marginTop: 16 },
+                      idx === arr.length - 1 && { marginBottom: 16 },
+                    ]}
+                    onPress={() => {
+                      if (isEditing) {
+                        setLocalWhiteList(wl => {
+                          if (inWhitelistLocal) {
+                            return wl.filter(
+                              w =>
+                                !addressUtils.isSameAddress(w, account.address),
+                            );
+                          } else {
+                            return [...wl, account.address];
+                          }
+                        });
+                      } else {
+                        onConfirm?.({
+                          name: account.aliasName,
+                          address: account.address,
+                        });
+                      }
+                    }}
+                  />
+                );
+              })}
+            </View>
+          </BottomSheetScrollView>
+        </BottomSheetView>
+      </AppBottomSheetModal>
+      <ModalConfirmDiscard
+        visible={isConfirmingDiscard}
+        onCancel={onCancelDiscard}
+        onConfirm={onConfirmedDiscard}
+      />
+    </>
   );
 }
 
