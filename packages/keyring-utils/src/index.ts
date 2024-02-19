69ec5a1ae7b476d1b4fc1a12d2488ec838bd6ac8
heisenberg <heisenberg-2077@outlook.com>
2024-02-19 11:11:53 +0800

    feat: ledger keyring

--- packages/keyring-utils/src/index.ts
+++ packages/keyring-utils/src/index.ts
@@ -4,11 +4,16 @@ import type { KeyringTypeName } from './types';
 export * from './types';
 export * from './account';
 export * from './intf';
+export * from './helper';
 
 export class DisplayKeyring {
   type: KeyringTypeName | string = '';
 
-  constructor(keyring: DisplayKeyring | (Partial<KeyringIntf> & { type: KeyringTypeName })) {
+  constructor(
+    keyring:
+      | DisplayKeyring
+      | (Partial<KeyringIntf> & { type: KeyringTypeName }),
+  ) {
     if (keyring instanceof DisplayKeyring) {
       // eslint-disable-next-line no-constructor-return
       return keyring;
