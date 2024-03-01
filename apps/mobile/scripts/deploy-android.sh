8c49f1381d842f87e6188263307130d8198d70e9
richardo2016x <richardo2016x@gmail.com>
2024-03-02 00:48:24 +0800

    build: android build.

--- apps/mobile/scripts/deploy-android.sh
+++ apps/mobile/scripts/deploy-android.sh
@@ -35,6 +35,8 @@ replace_variables $script_dir/tpl/android/version.json $android_deployments_dir/
 echo "[deploy-android] start build..."
 if [ $buildchannel == "appstore" ]; then
   version_bundle_suffix=".aab"
+  REALLY_UPLOAD="";
+  unset REALLY_UPLOAD;
   [ -z $SKIP_BUILD ] && sh $project_dir/android/build.sh buildAppStore
   [ -z $android_export_target ] && android_export_target="$project_dir/android/app/build/outputs/bundle/release/app-release.aab"
 else
