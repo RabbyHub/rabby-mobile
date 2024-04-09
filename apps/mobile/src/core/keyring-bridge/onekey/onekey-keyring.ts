9f28a9b9753137195a6a430b591591c4ba497974
heisenberg <heisenberg-2077@outlook.com>
2024-04-09 13:23:19 +0800

    fix: type

--- apps/mobile/src/core/keyring-bridge/onekey/onekey-keyring.ts
+++ apps/mobile/src/core/keyring-bridge/onekey/onekey-keyring.ts
@@ -1,6 +1,6 @@
 import { EthOneKeyKeyring } from '@rabby-wallet/eth-keyring-onekey';
 import OneKeyBridge from './onekey-bridge';
-console.log(111);
+
 export class OneKeyKeyring extends EthOneKeyKeyring {
   constructor() {
     super({
