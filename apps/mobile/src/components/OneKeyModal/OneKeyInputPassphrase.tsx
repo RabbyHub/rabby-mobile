cbad270956d6ef78857815573cf2498733c67875
heisenberg <heisenberg-2077@outlook.com>
2024-04-12 11:30:01 +0800

    feat: input pin modal

---
+++ apps/mobile/src/components/OneKeyModal/OneKeyInputPassphrase.tsx
@@ -0,0 +1,10 @@
+import { BottomSheetView } from '@gorhom/bottom-sheet';
+import { Text } from 'react-native';
+
+export const OneKeyInputPassphrase = () => {
+  return (
+    <BottomSheetView>
+      <Text>123</Text>
+    </BottomSheetView>
+  );
+};
--- apps/mobile/src/utils/onekey.ts
+++ apps/mobile/src/utils/onekey.ts
@@ -1,15 +1,53 @@
+import {
+  createGlobalBottomSheetModal,
+  removeGlobalBottomSheetModal,
+} from '@/components/GlobalBottomSheetModal';
+import { MODAL_NAMES } from '@/components/GlobalBottomSheetModal/types';
 import type { OneKeyKeyring } from '@/core/keyring-bridge/onekey/onekey-keyring';
 import { KeyringInstance } from '@rabby-wallet/service-keyring';
 import { eventBus, EVENTS } from './events';
 
 export function bindOneKeyEvents(keyring: KeyringInstance) {
-  (keyring as unknown as OneKeyKeyring).init();
+  const oneKeyKeyring = keyring as unknown as OneKeyKeyring;
+
+  oneKeyKeyring.init();
+  let pinModalId: string | null = null;
+  let passphraseModalId: string | null = null;
 
   eventBus.on(EVENTS.ONEKEY.REQUEST_PIN, () => {
-    // create a modal to receive pin
+    if (pinModalId) {
+      return;
+    }
+    pinModalId = createGlobalBottomSheetModal({
+      name: MODAL_NAMES.ONEKEY_INPUT_PIN,
+      onConfirm(pin: string, switchOnDevice: boolean) {
+        oneKeyKeyring.bridge.receivePin({
+          pin,
+          switchOnDevice,
+        });
+        eventBus.once(EVENTS.ONEKEY.CLOSE_UI_WINDOW, () => {
+          removeGlobalBottomSheetModal(pinModalId);
+          pinModalId = null;
+        });
+      },
+    });
   });
-
   eventBus.on(EVENTS.ONEKEY.REQUEST_PASSPHRASE, () => {
-    // create a modal to receive passphrase
+    if (passphraseModalId) {
+      return;
+    }
+    passphraseModalId = createGlobalBottomSheetModal({
+      name: MODAL_NAMES.ONEKEY_INPUT_PASSPHRASE,
+      onConfirm(passphrase: string, switchOnDevice: boolean) {
+        oneKeyKeyring.bridge.receivePassphrase({
+          passphrase,
+          switchOnDevice,
+        });
+        eventBus.once(EVENTS.ONEKEY.CLOSE_UI_WINDOW, () => {
+          removeGlobalBottomSheetModal(passphraseModalId);
+          passphraseModalId = null;
+        });
+      },
+    });
   });
 }
