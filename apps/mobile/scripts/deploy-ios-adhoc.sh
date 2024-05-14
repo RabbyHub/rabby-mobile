a3d3a28826c80df5268b957d526e82dd6e1931c6
richardo2016x <104543757+richardo2016x@users.noreply.github.com>
2024-05-14 18:13:09 +0800

    build: upgrade for Apple's compliance, and support upload ios archive to testflight on CI. (#265)
    * ci: support distribute ios appstore on ci.
    
    * ci: support upload_to_testflight automatically.
    
    * ci: fix.
    
    * ci: skip sentry sourcemap upload temporarily.
    
    * chore: code cleanup.
    
    * build: add `PrivacyInfo.xcprivacy` to for Apple's compliance
    
    * build: configure ruby version for rvm.
    
    * build: clean pod cache first on CI.

--- apps/mobile/scripts/deploy-ios-adhoc.sh
+++ apps/mobile/scripts/deploy-ios-adhoc.sh
@@ -5,7 +5,7 @@ project_dir=$(dirname $script_dir)
 
 . $script_dir/fns.sh --source-only
 
-[ -z $buildchannel ] && export buildchannel="selfhost-reg";
+export buildchannel="selfhost-reg";
 export BUILD_TARGET_PLATFORM="ios";
 check_build_params;
 check_s3_params;
@@ -17,28 +17,52 @@ proj_version=$(node --eval="process.stdout.write(require('./package.json').versi
 app_display_name=$(node --eval="process.stdout.write(require('./app.json').displayName)");
 cd $script_dir;
 
-ouput_dir=$project_dir/ios/Package/latest
-deployment_local_dir="$script_dir/deployments"
+ouput_dir=$project_dir/ios/Package/adhoc
+deployment_local_dir="$script_dir/deployments/ios-adhoc"
 
-tmp_dir="$script_dir/deployments/tmp"
-mkdir -p $tmp_dir
+rm -rf $deployment_local_dir && mkdir -p $deployment_local_dir;
 
-xcodebuild -project $project_dir/ios/RabbyMobile.xcodeproj -target "RabbyMobile" -showBuildSettings -json | plutil -convert xml1 - -o $tmp_dir/RabbyMobileAdHoc.plist
+mkdir -p "$script_dir/deployments/tmp"
+
+xcodebuild -project $project_dir/ios/RabbyMobile.xcodeproj -target "RabbyMobile" -showBuildSettings -json | plutil -convert xml1 - -o $script_dir/deployments/tmp/RabbyMobileAdHoc.plist
 
 ios_version_name=$(/usr/libexec/PlistBuddy -c "Print:CFBundleShortVersionString" $project_dir/ios/RabbyMobile/Info.plist)
-ios_version_code=$(/usr/libexec/PlistBuddy -c "Print:0:buildSettings:CURRENT_PROJECT_VERSION" $tmp_dir/RabbyMobileAdHoc.plist)
+ios_version_code=$(/usr/libexec/PlistBuddy -c "Print:0:buildSettings:CURRENT_PROJECT_VERSION" $script_dir/deployments/tmp/RabbyMobileAdHoc.plist)
 cd $project_dir/ios;
 
-unix_replace_variables $script_dir/tpl/ios/version.json $script_dir/deployments/ios/version.json \
+unix_replace_variables $script_dir/tpl/ios/version.json $deployment_local_dir/version.json \
   --var-DOWNLOAD_URL=$cdn_deployment_urlbase/ios/ \
   --var-APP_VER_CODE=$ios_version_code \
   --var-APP_VER="$proj_version"
 
-if [[ -z $SKIP_BUILD || ! -f $ouput_dir/RabbyMobile.ipa ]]; then
+# ============ prepare changelogs :start ============== #
+possible_changelogs=(
+  "$project_dir/src/changeLogs/$proj_version.md"
+)
+
+for changelog in "${possible_changelogs[@]}"; do
+  if [ -f $changelog ]; then
+    echo "[deploy-ios-adhoc] found changelog: $changelog"
+    cp $changelog $deployment_local_dir/
+    break
+  fi
+done
+# ============ prepare changelogs :end ============== #
+
+build_adhoc() {
+  cd $project_dir;
+  yarn;
+  yarn syncrnversion;
   cd $project_dir/ios;
   bundle install && bundle exec pod install;
   cd $project_dir;
   bundle exec fastlane ios adhoc;
+}
+
+if [[ -z $SKIP_BUILD || ! -f $ouput_dir/RabbyMobile.ipa ]]; then
+  echo "[deploy-ios-adhoc] start build..."
+  build_adhoc;
+  echo "[deploy-ios-adhoc] finish build."
 fi
 
 file_date=$(date -r $ouput_dir/RabbyMobile.ipa '+%Y%m%d_%H%M%S')
@@ -47,13 +71,13 @@ deployment_s3_dir=$S3_IOS_PUB_DEPLOYMENT/ios-$version_bundle_name
 deployment_cdn_baseurl=$cdn_deployment_urlbase/ios-$version_bundle_name
 manifest_plist_url="itms-services://?action=download-manifest&url=$deployment_cdn_baseurl/manifest.plist"
 
-cp $ouput_dir/RabbyMobile.ipa $deployment_local_dir/ios/rabbymobile.ipa
-cp $ouput_dir/manifest.plist $deployment_local_dir/ios/manifest.plist
+cp $ouput_dir/RabbyMobile.ipa $deployment_local_dir/rabbymobile.ipa
+cp $ouput_dir/manifest.plist $deployment_local_dir/manifest.plist
 
-/usr/libexec/PlistBuddy -c "Set:items:0:metadata:title Rabby Wallet" $deployment_local_dir/ios/manifest.plist
-/usr/libexec/PlistBuddy -c "Set:items:0:assets:0:url $deployment_cdn_baseurl/rabbymobile.ipa" $deployment_local_dir/ios/manifest.plist # appURL
-/usr/libexec/PlistBuddy -c "Set:items:0:assets:1:url $deployment_cdn_baseurl/icon_57x57@57w.png" $deployment_local_dir/ios/manifest.plist # displayImageURL
-/usr/libexec/PlistBuddy -c "Set:items:0:assets:2:url $deployment_cdn_baseurl/icon_512x512@512w.png" $deployment_local_dir/ios/manifest.plist # fullSizeImageURL
+/usr/libexec/PlistBuddy -c "Set:items:0:metadata:title Rabby Wallet" $deployment_local_dir/manifest.plist
+/usr/libexec/PlistBuddy -c "Set:items:0:assets:0:url $deployment_cdn_baseurl/rabbymobile.ipa" $deployment_local_dir/manifest.plist # appURL
+/usr/libexec/PlistBuddy -c "Set:items:0:assets:1:url $deployment_cdn_baseurl/icon_57x57@57w.png" $deployment_local_dir/manifest.plist # displayImageURL
+/usr/libexec/PlistBuddy -c "Set:items:0:assets:2:url $deployment_cdn_baseurl/icon_512x512@512w.png" $deployment_local_dir/manifest.plist # fullSizeImageURL
 
 echo "[deploy-ios-adhoc] will upload to $deployment_s3_dir"
 echo "[deploy-ios-adhoc] will be served at $deployment_cdn_baseurl"
@@ -62,10 +86,11 @@ echo ""
 
 if [ "$REALLY_UPLOAD" == "true" ]; then
   echo "[deploy-ios-adhoc] start sync to $deployment_s3_dir..."
-  aws s3 sync $script_dir/deployments/ios $deployment_s3_dir/ --exclude '*' --include "*.ipa" --acl public-read --content-type application/octet-stream
-  aws s3 sync $script_dir/deployments/ios $deployment_s3_dir/ --exclude '*' --include "*.plist" --acl public-read --content-type application/x-plist
-  aws s3 sync $script_dir/deployments/ios $deployment_s3_dir/ --exclude '*' --include "*.png" --acl public-read --content-type image/png
-  aws s3 sync $script_dir/deployments/ios $deployment_s3_dir/ --exclude '*' --include "*.json" --acl public-read --content-type application/json
+  aws s3 sync $deployment_local_dir $deployment_s3_dir/ --exclude '*' --include "*.ipa" --acl public-read --content-type application/octet-stream --exact-timestamps
+  aws s3 sync $deployment_local_dir $deployment_s3_dir/ --exclude '*' --include "*.plist" --acl public-read --content-type application/x-plist --exact-timestamps
+  aws s3 sync $deployment_local_dir $deployment_s3_dir/ --exclude '*' --include "*.png" --acl public-read --content-type image/png --exact-timestamps
+  aws s3 sync $deployment_local_dir $deployment_s3_dir/ --exclude '*' --include "*.json" --acl public-read --content-type application/json --exact-timestamps
+  # aws s3 sync $deployment_local_dir $deployment_s3_dir/ --exclude '*' --include "*.md" --acl public-read --content-type text/plain --exact-timestamps
 
   node $script_dir/notify-lark.js "$manifest_plist_url" ios
 fi
