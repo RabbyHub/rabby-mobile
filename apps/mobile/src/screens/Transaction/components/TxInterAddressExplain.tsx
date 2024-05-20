3079accc536f5840c2efd2433a762541e157ed0f
richardo2016x <104543757+richardo2016x@users.noreply.github.com>
2024-05-20 10:11:35 +0800

    style: tuning style of tx address explanation for "Approve" (#273)
    * style: tuning style of tx address explanation for "Approve"
    
    * chore: code normalization

--- apps/mobile/src/screens/Transaction/components/TxInterAddressExplain.tsx
+++ apps/mobile/src/screens/Transaction/components/TxInterAddressExplain.tsx
@@ -18,7 +18,6 @@ import { ALIAS_ADDRESS } from '@/constant/gas';
 import TouchableView from '@/components/Touchable/TouchableView';
 
 type TxInterAddressExplainProps = RNViewProps & {
-  actionTitleStyle?: StyleProp<TextStyle>;
   data: TxDisplayItem;
   isScam?: boolean;
 } & Pick<TxDisplayItem, 'cateDict' | 'projectDict' | 'tokenDict'>;
@@ -112,7 +111,6 @@ const getNameAndAddressStyle = createGetStyles(colors => {
 
 export const TxInterAddressExplain = ({
   style,
-  actionTitleStyle,
   data,
   projectDict,
   tokenDict,
@@ -129,7 +127,12 @@ export const TxInterAddressExplain = ({
   const projectNameNode = (
     <>
       {project?.name ? (
-        <Text style={[styles.projectNameText]}>{project.name}</Text>
+        <Text
+          style={[
+            !isApprove ? styles.projectNameText : styles.approveProjectText,
+          ]}>
+          {project.name}
+        </Text>
       ) : data.other_addr ? (
         <NameAndAddress address={data.other_addr} hideCopy={isScam} />
       ) : null}
@@ -140,9 +143,11 @@ export const TxInterAddressExplain = ({
 
   if (isCancel) {
     interAddressExplain = (
-      <Text style={StyleSheet.flatten([styles.actionTitle, actionTitleStyle])}>
-        {t('page.transactions.explain.cancel')}
-      </Text>
+      <View style={[styles.explain]}>
+        <Text style={StyleSheet.flatten([styles.actionTitle])}>
+          {t('page.transactions.explain.cancel')}
+        </Text>
+      </View>
     );
   } else if (isApprove) {
     const tokenId = data.token_approve?.token_id || '';
@@ -153,25 +158,27 @@ export const TxInterAddressExplain = ({
     const amount = data.token_approve?.value || 0;
 
     interAddressExplain = (
-      <Text
-        style={StyleSheet.flatten([styles.actionTitle, actionTitleStyle])}
-        numberOfLines={1}>
-        Approve {amount < 1e9 ? amount.toFixed(4) : 'infinite'}{' '}
-        {`${getTokenSymbol(approveToken)} for `}
+      <View style={[styles.explain, isApprove && styles.explainForApprove]}>
+        <Text
+          style={StyleSheet.flatten([styles.actionTitle])}
+          numberOfLines={1}>
+          Approve {amount < 1e9 ? amount.toFixed(4) : 'infinite'}{' '}
+          {`${getTokenSymbol(approveToken)} for `}
+        </Text>
         {projectNameNode}
-      </Text>
+      </View>
     );
   } else {
     interAddressExplain = (
-      <>
+      <View style={[styles.explain]}>
         <Text
-          style={StyleSheet.flatten([styles.actionTitle, actionTitleStyle])}
+          style={StyleSheet.flatten([styles.actionTitle])}
           numberOfLines={1}>
           {cateDict[data.cate_id || '']?.name ??
             (data.tx?.name || t('page.transactions.explain.unknown'))}
         </Text>
         <View style={styles.actionDesc}>{projectNameNode}</View>
-      </>
+      </View>
     );
   }
 
@@ -181,9 +188,14 @@ export const TxInterAddressExplain = ({
         src={projectDict[data.project_id as string]?.logo_url}
         cateId={data.cate_id}
       />
-      <View style={styles.explain}>{interAddressExplain}</View>
+      {interAddressExplain}
     </View>
   );
+};
+
+const actionFont = {
+  fontSize: 15,
+  lineHeight: 18,
 };
 
 const getStyles = (colors: AppColorsVariants) =>
@@ -199,9 +211,15 @@ const getStyles = (colors: AppColorsVariants) =>
       maxWidth: '100%',
       // ...makeDebugBorder('red'),
     },
+    explainForApprove: {
+      width: '100%',
+      flexShrink: 0,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+    },
     actionTitle: {
-      fontSize: 15,
-      lineHeight: 18,
+      ...actionFont,
       color: colors['neutral-title1'],
       marginBottom: 4,
       maxWidth: '100%',
@@ -214,5 +232,10 @@ const getStyles = (colors: AppColorsVariants) =>
       fontSize: 12,
       lineHeight: 14,
       color: colors['neutral-foot'],
+    },
+    approveProjectText: {
+      position: 'relative',
+      top: -2,
+      ...actionFont,
     },
   });
