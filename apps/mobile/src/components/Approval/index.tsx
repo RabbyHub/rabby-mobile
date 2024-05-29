dabc061099bcfb4aa6f235a1999792b7fb7d6ac6
heisenberg <110591045+heisenberg-2077@users.noreply.github.com>
2024-05-29 22:04:01 +0800

    feat: support safe (#280)
    * fix: reload approval
    
    * fix: sign

--- apps/mobile/src/components/Approval/index.tsx
+++ apps/mobile/src/components/Approval/index.tsx
@@ -1,7 +1,12 @@
+import { notificationService } from '@/core/services';
 import { useApproval } from '@/hooks/useApproval';
+import { eventBus, EVENT_ACTIVE_WINDOW } from '@/utils/events';
 import { IExtractFromPromise } from '@/utils/type';
+import events from 'events';
 import React from 'react';
 import { View } from 'react-native';
+import { removeGlobalBottomSheetModal } from '../GlobalBottomSheetModal';
+import { EVENT_NAMES } from '../GlobalBottomSheetModal/types';
 
 import * as ApprovalComponent from './components';
 
@@ -13,19 +18,30 @@ export const Approval = () => {
   >;
   const [approval, setApproval] = React.useState<IApproval | null>(null);
 
-  const init = async () => {
-    const _approval = await getApproval();
-    if (!_approval) {
-      // history.replace('/');
-      return null;
-    }
-    setApproval(_approval);
-  };
+  const init = React.useCallback(
+    async (winId?: string) => {
+      const _approval = await getApproval();
+      if (!_approval) {
+        removeGlobalBottomSheetModal(winId);
+        return null;
+      }
+      setApproval(_approval);
+    },
+    [getApproval],
+  );
 
   React.useEffect(() => {
     init();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  React.useEffect(() => {
+    eventBus.on(EVENT_ACTIVE_WINDOW, init);
+
+    return () => {
+      eventBus.off(EVENT_ACTIVE_WINDOW, init);
+    };
+  }, [init]);
 
   if (!approval) {
     return <View />;
