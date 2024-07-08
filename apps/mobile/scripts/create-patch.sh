54cd80429f0e7bd17f618445a755eb0498f0f067
richardo2016x <richardo2016x@gmail.com>
2024-07-08 23:57:37 +0800

    security: interrupt camera/location permission request and ask user.

--- apps/mobile/scripts/create-patch.sh
+++ apps/mobile/scripts/create-patch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # ./node_modules/.bin/patch-package @isudaji/react-native-install-apk --exclude 'build|xcodeproj|package.json'
-# ./node_modules/.bin/patch-package react-native-webview --exclude 'build|xcodeproj|package.json' --include 'android'
+./node_modules/.bin/patch-package react-native-webview --exclude 'lib|build|xcodeproj|package.json' --include 'android\/.*\/(RNCWebChromeClient|RNCWebViewClient)\.java|apple\/(RNCWebViewImpl|RNCWebView)\.m'
 ./node_modules/.bin/patch-package @debank/common
 ./node_modules/.bin/patch-package react-native-mmkv --exclude 'android\/.cxx|build|xcodeproj|package.json'
