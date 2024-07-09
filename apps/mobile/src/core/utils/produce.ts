729ad8a1c9316c199d6c30b58fb3e258dfa4fbb9
richardo2016x <richardo2016x@gmail.com>
2024-07-09 15:46:55 +0800

    feat: type of produce

--- apps/mobile/src/core/utils/produce.ts
+++ apps/mobile/src/core/utils/produce.ts
@@ -1,6 +1,13 @@
 import { clone } from 'lodash';
 
-export const produce = (base, producer) => {
+/**
+ * @description produce a new object from the base object.
+ *
+ * shallow clone the base object and apply the producer function to the clone object
+ *
+ * @see https://immerjs.github.io/immer/produce/
+ */
+export const produce = <T = any>(base: T, producer: (draft: T) => void) => {
   const cloneObj = clone(base);
   producer(cloneObj);
   return cloneObj;
