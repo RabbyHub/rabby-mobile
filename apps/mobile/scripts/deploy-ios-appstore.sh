7e55480c5f9cbb7e80b7e528c108285866e1c03c
richardo2016x <richardo2016x@gmail.com>
2024-07-10 20:06:49 +0800

    build: try to fix ios build.

--- apps/mobile/scripts/deploy-ios-appstore.sh
+++ apps/mobile/scripts/deploy-ios-appstore.sh
@@ -55,9 +55,8 @@ build_appstore() {
   yarn;
   yarn syncrnversion;
   cd $project_dir/ios;
-  bundle install;
   [ ! -z $CI ] && bundle exec pod cache clean --all;
-  bundle exec pod install;
+  bundle install && bundle exec pod install;
   cd $project_dir;
   bundle exec fastlane ios appstore;
 }
