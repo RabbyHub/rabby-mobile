6e49060973852bf5081af2065676caed9292cf67
richardo2016x <richardo2016x@gmail.com>
2024-07-16 15:29:52 +0800

    fix: form errors.

--- apps/mobile/src/utils/patch.ts
+++ apps/mobile/src/utils/patch.ts
@@ -1,4 +1,5 @@
-import type { FormikConfig, FormikValues, useFormik } from 'formik';
+import * as Yup from 'yup';
+import { FormikConfig, FormikErrors, FormikValues, useFormik } from 'formik';
 
 // export function resetFormikFieldError<Values extends FormikValues = FormikValues>(formik: FormikConfig<FormikValues>, field: string) {
 //   formik.set
@@ -38,6 +39,36 @@ export function setFieldValueAndTouched<
   >,
   touched = true,
 ) {
-  formik.setFieldTouched(field, touched);
+  formik.setFieldTouched(field, touched, shouldValidte);
   formik.setFieldValue(field, value, shouldValidte);
+}
+
+export function validateFormikSchema<
+  Values extends FormikValues = FormikValues,
+>(
+  formikValues: Values,
+  yupSchema: Yup.ObjectSchema<Values>,
+): FormikErrors<Values> {
+  let errors = {};
+  try {
+    yupSchema.validateSync(formikValues, { abortEarly: true });
+  } catch (error: any) {
+    errors[error.path] = error.message;
+  }
+
+  return errors;
+}
+
+export function useAppFormik<Values extends FormikValues = FormikValues>(
+  options: FormikConfig<Values>,
+) {
+  type Orig = ReturnType<typeof useFormik<Values>>;
+  const formik = useFormik<Values>(options) as Orig & {
+    validateFormValues: () => ReturnType<typeof validateFormikSchema<Values>>;
+  };
+
+  formik.validateFormValues = (values?: Values) =>
+    validateFormikSchema(values ?? formik.values, options.validationSchema);
+
+  return formik;
 }
