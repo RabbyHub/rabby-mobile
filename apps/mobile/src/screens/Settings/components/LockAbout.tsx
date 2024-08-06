1d1ff6ba494eb1a8bbb99f8a7605a2cbcd01e148
richardo2016x <richardo2016x@gmail.com>
2024-08-06 19:10:48 +0800

    chore: code cleanup.

--- apps/mobile/src/screens/Settings/components/LockAbout.tsx
+++ apps/mobile/src/screens/Settings/components/LockAbout.tsx
@@ -1,22 +1,5 @@
 import React, { useRef } from 'react';
-import {
-  Animated as RNAnimated,
-  Dimensions,
-  PanResponder,
-  Text,
-  View,
-} from 'react-native';
-import {
-  Gesture,
-  GestureDetector,
-  GestureHandlerRootView,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-} from 'react-native-gesture-handler';
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-} from 'react-native-reanimated';
+import { Text, View } from 'react-native';
 
 import { useAutoLockTime, useLastUnlockTime } from '@/hooks/appTimeout';
 import { useThemeColors, useThemeStyles } from '@/hooks/theme';
@@ -24,16 +7,12 @@ import useInterval from 'react-use/lib/useInterval';
 import { NEED_DEVSETTINGBLOCKS } from '@/constant/env';
 import { getTimeSpan, getTimeSpanByMs } from '@/utils/time';
 import { usePasswordStatus } from '@/hooks/useLock';
-import { colord } from 'colord';
 import { createGetStyles, makeDebugBorder } from '@/utils/styles';
 import {
   useAutoLockTimeMinites,
-  useFloatingView,
   useToggleShowAutoLockCountdown,
 } from '@/hooks/appSettings';
 import { TIME_SETTINGS } from '@/constant/autoLock';
-import TouchableView from '@/components/Touchable/TouchableView';
-import { RcIconLogo } from '@/assets/icons/common';
 
 export function useAutoLockCountDown() {
   const { autoLockTime } = useAutoLockTime();
@@ -141,161 +120,6 @@ export function AutoLockSettingLabel() {
     </Text>
   );
 }
-
-function clamp(val: number, min: number, max: number) {
-  return Math.min(Math.max(val, min), max);
-}
-const VIEW_W = 240;
-const DRAGGER_SIZE = 60;
-const INIT_RIGHT = VIEW_W - DRAGGER_SIZE;
-const INIT_LAYOUT = {
-  top: 100,
-  // left: Dimensions.get('window').width - INIT_RIGHT,
-};
-const screenLayout = Dimensions.get('screen');
-export function FloatViewAutoLockCount() {
-  const { styles } = useThemeStyles(getFloatViewAutoLockCountStyles);
-  const { countDownText, textColor } = useAutoLockCountDown();
-  const { collapsed, toggleCollapsed, shouldShow } = useFloatingView();
-
-  const [translationX, translationY, prevTranslationX, prevTranslationY] = [
-    useSharedValue(0),
-    useSharedValue(0),
-    useSharedValue(0),
-    useSharedValue(0),
-  ];
-
-  const animatedStyles = useAnimatedStyle(() => {
-    return {
-      transform: [{ translateY: translationY.value }],
-    };
-  });
-
-  const composedGesture = React.useMemo(() => {
-    const tap = Gesture.Tap()
-      .runOnJS(true)
-      .onEnd(() => {
-        toggleCollapsed();
-      });
-
-    const pan = Gesture.Pan()
-      .minDistance(5)
-      .enabled(true)
-      .runOnJS(true)
-      .onStart(() => {
-        prevTranslationX.value = translationX.value;
-        prevTranslationY.value = translationY.value;
-      })
-      .onUpdate(event => {
-        try {
-          const maxTranslateX = screenLayout.width / 2 - 50;
-          const maxTranslateY = screenLayout.height / 2 - 50;
-
-          translationX.value = clamp(
-            prevTranslationX.value + event.translationX,
-            -maxTranslateX,
-            maxTranslateX,
-          );
-          translationY.value = clamp(
-            prevTranslationY.value + event.translationY,
-            -maxTranslateY,
-            maxTranslateY,
-          );
-        } catch (err: any) {
-          console.error('onUpdate error', err?.message);
-        }
-      });
-
-    const composed = Gesture.Race(...[pan, tap]);
-    return composed;
-  }, [
-    toggleCollapsed,
-    translationX,
-    translationY,
-    prevTranslationX,
-    prevTranslationY,
-  ]);
-
-  if (!NEED_DEVSETTINGBLOCKS) return null;
-  if (!shouldShow) return null;
-
-  return (
-    <GestureHandlerRootView
-      style={[styles.gestureContainer, !collapsed && styles.containerExpanded]}>
-      <Animated.View
-        style={[
-          styles.container,
-          animatedStyles,
-          // {
-          //   top: dragPosRef.current.getLayout().top
-          // },
-        ]}>
-        <GestureDetector gesture={composedGesture}>
-          {/* dragger */}
-          <TouchableOpacity style={styles.dragger}>
-            <RcIconLogo width={48} height={48} />
-          </TouchableOpacity>
-        </GestureDetector>
-        <View pointerEvents="none" style={[styles.animatedView]}>
-          {countDownText && <Text style={styles.label}>Auto Lock after </Text>}
-          <Text style={{ color: textColor, fontWeight: '600' }}>
-            {countDownText || 'Time Reached'}
-          </Text>
-        </View>
-      </Animated.View>
-    </GestureHandlerRootView>
-  );
-}
-
-const getFloatViewAutoLockCountStyles = createGetStyles(colors => {
-  return {
-    gestureContainer: {
-      flex: 1,
-      position: 'absolute',
-      // ...makeDebugBorder(),
-      zIndex: 999,
-      width: VIEW_W,
-      top: INIT_LAYOUT.top,
-      height: 60,
-      right: -INIT_RIGHT,
-    },
-    container: {
-      flex: 1,
-      position: 'absolute',
-      borderRadius: 6,
-      flexDirection: 'row',
-      justifyContent: 'flex-start',
-      alignItems: 'center',
-    },
-    dragger: {
-      borderTopLeftRadius: 60,
-      borderBottomLeftRadius: 60,
-      height: 60,
-      width: 60,
-      flexShrink: 0,
-      opacity: 0.5,
-      backgroundColor: colors['blue-default'],
-      // ...makeDebugBorder('yellow'),
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    containerExpanded: {
-      right: 0,
-    },
-    animatedView: {
-      backgroundColor: colord('#000000').alpha(0.5).toRgbString(),
-      flexShrink: 1,
-      width: '100%',
-      height: '100%',
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexDirection: 'row',
-    },
-    label: {
-      color: colord('#ffffff').alpha(0.8).toRgbString(),
-    },
-  };
-});
 
 export function LastUnlockTimeLabel() {
   const { unlockTime } = useLastUnlockTime();
