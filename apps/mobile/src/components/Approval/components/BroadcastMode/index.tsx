0254402619c33c452e7d3a0deafbc85b7bf5f7db
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2024-08-08 10:55:21 +0800

    feat: remove gasless mode

--- apps/mobile/src/components/Approval/components/BroadcastMode/index.tsx
+++ apps/mobile/src/components/Approval/components/BroadcastMode/index.tsx
@@ -130,13 +130,6 @@ export const BroadcastMode = ({
       value: 'default',
     },
     {
-      title: t('page.signTx.BroadcastMode.lowGas.title'),
-      desc: t('page.signTx.BroadcastMode.lowGas.desc'),
-      value: 'low_gas',
-      disabled: disabledMap.low_gas.disabled,
-      tips: disabledMap.low_gas.tips,
-    },
-    {
       title: t('page.signTx.BroadcastMode.mev.title'),
       desc: t('page.signTx.BroadcastMode.mev.desc'),
       value: 'mev',
