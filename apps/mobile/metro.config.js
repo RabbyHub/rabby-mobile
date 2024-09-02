0622582c1bcb48a474e229c766ac77cb4375c98f
heisenberg <heisenberg-2077@outlook.com>
2024-09-02 10:29:41 +0800

    fix: ledger ble2

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,9 +1,13 @@
+// @ts-ignore
 const path = require('path');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 const {
   createSentryMetroSerializer,
 } = require('@sentry/react-native/dist/js/tools/sentryMetroSerializer');
+
+const defaultModuleResolver =
+  getDefaultConfig(__dirname).resolver.resolveRequest;
 
 /**
  * Metro configuration
@@ -36,6 +40,62 @@ const getAppConfig = function () {
     extraNodeModules: require('node-libs-react-native'),
     assetExts: resolver.assetExts.filter(ext => ext !== 'svg'),
     sourceExts: [...resolver.sourceExts, 'svg'],
+    /**
+     * fix ledger import issue
+     * https://github.com/LedgerHQ/ledger-live/issues/6173#issuecomment-2008939013
+     *
+     * */
+    resolveRequest: (context, moduleName, platform) => {
+      try {
+        return context.resolveRequest(context, moduleName, platform);
+      } catch (error) {
+        console.warn(
+          '\n1️⃣ context.resolveRequest cannot resolve: ',
+          moduleName,
+        );
+      }
+
+      try {
+        const resolution = require.resolve(moduleName, {
+          paths: [
+            path.dirname(context.originModulePath),
+            ...config.resolver.nodeModulesPaths,
+          ],
+        });
+
+        if (path.isAbsolute(resolution)) {
+          return {
+            filePath: resolution,
+            type: 'sourceFile',
+          };
+        }
+      } catch (error) {
+        console.warn('\n2️⃣ require.resolve cannot resolve: ', moduleName);
+      }
+
+      try {
+        return defaultModuleResolver(context, moduleName, platform);
+      } catch (error) {
+        console.warn('\n3️⃣ defaultModuleResolver cannot resolve: ', moduleName);
+      }
+
+      try {
+        return {
+          filePath: require.resolve(moduleName),
+          type: 'sourceFile',
+        };
+      } catch (error) {
+        console.warn('\n4️⃣ require.resolve cannot resolve: ', moduleName);
+      }
+
+      try {
+        const resolution = getDefaultConfig(require.resolve(moduleName))
+          .resolver?.resolveRequest;
+        return resolution(context, moduleName, platform);
+      } catch (error) {
+        console.warn('\n5️⃣ getDefaultConfig cannot resolve: ', moduleName);
+      }
+    },
   };
   config.watchFolders = [
     path.resolve(__dirname, '../../node_modules'),
