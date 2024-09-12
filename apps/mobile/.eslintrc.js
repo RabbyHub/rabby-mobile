e19cf494276b1d2c25305b4a2a213803ab7c59f4
DMY <147dmy@gmail.com>
2024-09-12 11:25:02 +0800

    chore: lint react-use

--- apps/mobile/.eslintrc.js
+++ apps/mobile/.eslintrc.js
@@ -6,5 +6,13 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react-hooks/exhaustive-deps': 'error',
     '@typescript-eslint/no-unused-vars': 'warn',
+    'no-restricted-imports': [
+      'error',
+      {
+        name: 'react-use',
+        message:
+          "Please import the specific function from react-use instead of the whole library (e.g. import useTimeout from 'react-use/lib/useTimeout')",
+      },
+    ],
   },
 };
