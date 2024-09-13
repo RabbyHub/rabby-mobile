86ff3d87448febd6e7a18ca82c21f2e51342cdbc
heisenberg <110591045+heisenberg-2077@users.noreply.github.com>
2024-09-13 18:24:12 +0800

    fix: disable style for sign button (#422)
    * fix: disable style for sign button
    
    * fix: process button style

--- apps/mobile/src/components/Approval/components/FooterBar/ProcessActions.tsx
+++ apps/mobile/src/components/Approval/components/FooterBar/ProcessActions.tsx
@@ -70,32 +70,34 @@ export const ProcessActions: React.FC<Props> = ({
 
   return (
     <ActionsContainer onCancel={onCancel}>
-      <Tip
-        // @ts-expect-error
-        content={tooltipContent}>
-        <View>
-          <GasLessAnimatedWrapper
-            isGasNotEnough={isGasNotEnough}
-            gasLessThemeColor={gasLessThemeColor}
-            title={buttonText}
-            titleStyle={buttonTextStyle}
-            buttonStyle={buttonStyle}
-            gasLess={gasLess}
-            showOrigin={!gasLess && !disabledProcess}
-            type="process">
-            <Button
-              disabled={disabledProcess}
-              type={buttonIsPrimary ? 'primary' : 'clear'}
-              buttonStyle={[styles.button, buttonStyle]}
-              titleStyle={buttonTextStyle}
-              disabledStyle={styles.disabled}
-              onPress={onSubmit}
+      <View>
+        <Tip
+          // @ts-expect-error
+          content={tooltipContent}>
+          <View>
+            <GasLessAnimatedWrapper
+              isGasNotEnough={isGasNotEnough}
+              gasLessThemeColor={gasLessThemeColor}
               title={buttonText}
-              showTitleOnLoading
-            />
-          </GasLessAnimatedWrapper>
-        </View>
-      </Tip>
+              titleStyle={buttonTextStyle}
+              buttonStyle={buttonStyle}
+              gasLess={gasLess}
+              showOrigin={!gasLess && !disabledProcess}
+              type="process">
+              <Button
+                disabled={disabledProcess}
+                type={buttonIsPrimary ? 'primary' : 'clear'}
+                buttonStyle={[styles.button, buttonStyle]}
+                titleStyle={buttonTextStyle}
+                disabledStyle={styles.disabled}
+                onPress={onSubmit}
+                title={buttonText}
+                showTitleOnLoading
+              />
+            </GasLessAnimatedWrapper>
+          </View>
+        </Tip>
+      </View>
     </ActionsContainer>
   );
 };
