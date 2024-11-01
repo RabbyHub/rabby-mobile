6aa1e4ed7702bcac8cb8f8049ac20dc7aa1e6d50
hz002 <zs4733525@gmail.com>
2024-11-01 16:38:27 +0800

    fix: remove text out Text

--- apps/mobile/src/screens/RestoreFromCloud/RestoreFromCloud2024.tsx
+++ apps/mobile/src/screens/RestoreFromCloud/RestoreFromCloud2024.tsx
@@ -125,7 +125,6 @@ export const RestoreFromCloud2024: React.FC<{
           </Text>
         </View>
         <View style={styles.loadingList}>
-          Loading
           <BackupItemSkeleton />
           <BackupItemSkeleton />
         </View>
