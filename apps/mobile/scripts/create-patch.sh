a77fb5681c9e60e95f1c61623a1e1d0c3a913023
richardo2016x <richardo2016x@gmail.com>
2024-11-13 18:56:14 +0800

    build: fix podfile.lock

--- apps/mobile/scripts/create-patch.sh
+++ apps/mobile/scripts/create-patch.sh
@@ -4,7 +4,7 @@
 ./node_modules/.bin/patch-package react-native-webview --exclude 'build|xcodeproj|package.json' --include 'lib\/.*\.d\.ts|android\/src\/.*\/.*(\.java|\.kt)|apple\/.*(\.mm?|\.h)'
 ./node_modules/.bin/patch-package @debank/common
 ./node_modules/.bin/patch-package react-native-fs --exclude 'android\/build' --include 'android'
-./node_modules/.bin/patch-package react-native-mmkv --exclude 'android\/.cxx|build|xcodeproj|package.json'
+./node_modules/.bin/patch-package react-native-mmkv --exclude 'android\/.cxx|build|xcodeproj|package.json' --include '.*\.cpp|.*\.mm?|.*\.(js|ts)|.*\.podspec'
 # ./node_modules/.bin/patch-package @onekeyfe/react-native-ble-plx --exclude 'android\/.cxx|build|xcodeproj|package.json'
 # ./node_modules/.bin/patch-package react-native-ble-plx --exclude 'android\/.cxx|build|xcodeproj|package.json'
 ./node_modules/.bin/patch-package @onekeyfe/hd-transport-react-native --exclude 'android\/.cxx|build|xcodeproj|package.json'
