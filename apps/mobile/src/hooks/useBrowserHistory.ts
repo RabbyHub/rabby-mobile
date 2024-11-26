a6a9bf7a6c11af22128098e407dc621fc5756e76
richardo2016x <richardo2016x@gmail.com>
2024-11-26 23:22:40 +0800

    feat: filter out history items older than 30 days.

--- apps/mobile/src/hooks/useBrowserHistory.ts
+++ apps/mobile/src/hooks/useBrowserHistory.ts
@@ -6,8 +6,22 @@ import { DappInfo } from '@/core/services/dappService';
 import { dappsAtom } from '@/core/storage/serviceStoreStub';
 import { useMemoizedFn } from 'ahooks';
 import { atom, useAtom } from 'jotai';
+import { isNonPublicProductionEnv } from '@/constant/env';
 
 const browserHistoryAtom = atom<BrowserHistoryItem[]>([]);
+
+const BROWSER_HISTORY_LIMIT_SHORT = {
+  // maxCount: 10,
+  expireDuration: 10 * 60 * 1000, // 10 minutes
+};
+const BROWSER_HISTORY_LIMIT = {
+  // maxCount: 100,
+  expireDuration: 30 * 24 * 60 * 60 * 1000, // 30 days
+};
+
+const EXPIRE_DURATION = !isNonPublicProductionEnv
+  ? BROWSER_HISTORY_LIMIT.expireDuration
+  : BROWSER_HISTORY_LIMIT_SHORT.expireDuration;
 
 export function useBrowserHistory() {
   const [dapps] = useAtom(dappsAtom);
@@ -32,7 +46,9 @@ export function useBrowserHistory() {
   const browserHistoryList = useMemo(() => {
     const res: DappInfo[] = [];
     historyStore.forEach(item => {
-      if (dapps[item.origin]) {
+      // filter dapps older then 30 days
+      const favoriteDiff = Date.now() - item.createdAt;
+      if (dapps[item.origin] && favoriteDiff <= EXPIRE_DURATION) {
         res.push(dapps[item.origin]);
       }
     });
