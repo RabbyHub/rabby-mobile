229b4bbca5ae30fd94f76eef85976362075519ec
richardo2016x <richardo2016x@gmail.com>
2024-11-27 11:54:53 +0800

    style: tuning style for Send to-address-input

--- apps/mobile/src/screens/Send/components/ToAddressControl.tsx
+++ apps/mobile/src/screens/Send/components/ToAddressControl.tsx
@@ -170,7 +170,7 @@ export default function ToAddressControl({
         inputStyle={[styles.input, !formValues.to && styles.inputWithoutValue]}
         hasError={!!errors.to}
         clearable={false}
-        customIcon={CustomIcon}
+        // customIcon={CustomIcon}
         inputProps={{
           ...inputProps,
           multiline: true,
@@ -179,7 +179,7 @@ export default function ToAddressControl({
             handleFieldChange('to', value);
           },
           onBlur: formik.handleBlur('to'),
-          placeholder: 'Enter an address or search',
+          placeholder: 'Enter an address',
           placeholderTextColor: colors2024['neutral-info'],
           style: {
             paddingTop: 0,
