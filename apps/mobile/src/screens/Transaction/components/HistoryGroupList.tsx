7c30bd66f327da78f88bf5f168732aa132d1c53b
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2024-11-27 12:10:04 +0800

    fix: address display

--- apps/mobile/src/screens/Transaction/components/HistoryGroupList.tsx
+++ apps/mobile/src/screens/Transaction/components/HistoryGroupList.tsx
@@ -56,6 +56,9 @@ function markFirstItems(
           newItem.isDateStart = true;
         }
       }
+      if (newItem.isDateStart && !newItem.isFirst) {
+        newItem.isFirst = true;
+      }
     }
     newArr.push(newItem);
   }
