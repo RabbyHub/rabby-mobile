08e448ed95f6f962a42e705fdc0cd2d7821371aa
richardo2016x <richardo2016x@gmail.com>
2024-12-02 15:36:55 +0800

    build: patch @ledgerhq/hw-app-eth

--- apps/mobile/scripts/create-patch.sh
+++ apps/mobile/scripts/create-patch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+## at apps/mobile
 # ./node_modules/.bin/patch-package @isudaji/react-native-install-apk --exclude 'build|xcodeproj|package.json'
 ./node_modules/.bin/patch-package react-native-webview --exclude 'build|xcodeproj|package.json' --include 'lib\/.*\.d\.ts|android\/src\/.*\/.*(\.java|\.kt)|apple\/.*(\.mm?|\.h)'
 ./node_modules/.bin/patch-package @debank/common
@@ -8,3 +9,6 @@
 # ./node_modules/.bin/patch-package @onekeyfe/react-native-ble-plx --exclude 'android\/.cxx|build|xcodeproj|package.json'
 # ./node_modules/.bin/patch-package react-native-ble-plx --exclude 'android\/.cxx|build|xcodeproj|package.json'
 ./node_modules/.bin/patch-package @onekeyfe/hd-transport-react-native --exclude 'android\/.cxx|build|xcodeproj|package.json'
+
+## at monorepo root
+./node_modules/.bin/patch-package @ledgerhq/hw-app-eth --include 'lib\/services\/ledger/(erc20|contracts)\.js$'
