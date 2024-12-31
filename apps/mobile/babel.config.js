145d377503126f5f16ed72312f8ffa21c9f0a4b5
richardo2016x <richardo2016x@gmail.com>
2024-12-31 17:54:39 +0800

    build: more git details on build.

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,12 +2,50 @@ const child_process = require('child_process');
 
 const pkg = require('./package.json');
 
-const BUILD_GIT_HASH = child_process
-  .execSync('git log --format="%h" -n 1')
-  .toString()
-  .trim();
-
 const { version } = pkg;
+
+const buildGitInfo = (function getBuildEnvVars() {
+  const BUILD_GIT_HASH = child_process
+    .execSync('git log --format="%h" -n 1')
+    .toString()
+    .trim();
+
+  const BUILD_GIT_HASH_TIME = child_process
+    .execSync(
+      `TZ=UTC0 git show --quiet --date='format-local:%Y-%m-%dT%H:%M:%S+00:00' --format="%cd"`,
+    )
+    .toString()
+    .trim();
+
+  const buildchannel = process.env.buildchannel || 'selfhost-reg';
+
+  const BUILD_GIT_COMMITOR =
+    buildchannel !== 'selfhost-reg'
+      ? ''
+      : child_process
+          // format: name
+          .execSync('git show --quiet --format="%cn"')
+          // // format: name(email)
+          // .execSync('git log -1 --pretty=format:"%an(%ae)"')
+          .toString()
+          .trim();
+
+  const BUILD_GIT_COMMITS_COUNT_BASEVER = version;
+
+  // calculate commits count from v{version}
+  const BUILD_GIT_COMMITS_COUNT = child_process
+    .execSync(`git rev-list --count v${BUILD_GIT_COMMITS_COUNT_BASEVER}..HEAD`)
+    .toString()
+    .trim();
+
+  return {
+    BUILD_GIT_HASH,
+    BUILD_GIT_HASH_TIME,
+    BUILD_GIT_COMMITOR,
+    BUILD_GIT_COMMITS_COUNT,
+    BUILD_GIT_COMMITS_COUNT_BASEVER,
+  };
+})();
 
 module.exports = {
   presets: [
@@ -24,7 +62,14 @@ module.exports = {
         'process.env.BUILD_TIME': new Date().toISOString(),
         'process.env.BUILD_ENV': process.env.BUILD_ENV || 'production',
         'process.env.buildchannel': process.env.buildchannel || 'selfhost-reg',
-        'process.env.BUILD_GIT_HASH': BUILD_GIT_HASH,
+        'process.env.BUILD_GIT_INFO': JSON.stringify({
+          BUILD_GIT_HASH: buildGitInfo.BUILD_GIT_HASH,
+          BUILD_GIT_HASH_TIME: buildGitInfo.BUILD_GIT_HASH_TIME,
+          BUILD_GIT_COMMITOR: buildGitInfo.BUILD_GIT_COMMITOR,
+          BUILD_GIT_COMMITS_COUNT: buildGitInfo.BUILD_GIT_COMMITS_COUNT,
+          BUILD_GIT_COMMITS_COUNT_BASEVER:
+            buildGitInfo.BUILD_GIT_COMMITS_COUNT_BASEVER,
+        }),
       },
     ],
     [
