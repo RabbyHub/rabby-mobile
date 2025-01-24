ab884c6f019ead2ec963c183e996119ff84e15fa
heisenberg <110591045+heisenberg-2077@users.noreply.github.com>
2025-01-24 16:17:04 +0800

    feat: add sendPersonalMessage and support gas account no need sign login (#566)
    * feat: add sendPersonalMessage
    
    * Update sendPersonalMessage.ts
    
    * fix: no need sign account type
    
    * fix: remove hash log
    
    ---------
    
    Co-authored-by: jinming <jinming0228@qq.com>

--- apps/mobile/src/core/apis/provider.ts
+++ apps/mobile/src/core/apis/provider.ts
@@ -280,6 +280,12 @@ export const ethSendTransaction = async (
   }
 };
 
+export const ethPersonalSign = async (
+  ...args: Parameters<typeof providerController.personalSign>
+) => {
+  return providerController.personalSign(...args);
+};
+
 type gasMarketV2ParamsV2 = {
   chain: Chain;
   tx: Tx;
