6b7764756208fca2859505018925978bed936c0e
Hodor <lost1q84@gmail.com>
2025-02-12 15:59:25 +0800

    fix: token detail token price 24h change

--- apps/mobile/src/screens/Home/components/AssetRenderItems/TokenRenderItems.tsx
+++ apps/mobile/src/screens/Home/components/AssetRenderItems/TokenRenderItems.tsx
@@ -38,7 +38,7 @@ const formatPercentage = (x: number) => {
   if (Math.abs(x) < 0.00001) {
     return '0%';
   }
-  const percentage = (x * 100).toFixed(3);
+  const percentage = (x * 100).toFixed(2);
   return `${x >= 0 ? '+' : ''}${percentage}%`;
 };
 
