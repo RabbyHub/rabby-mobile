d90bc42b4e35b7380bf93c5581cbe698291397c5
DMY <147dmy@gmail.com>
2025-02-24 16:24:43 +0800

    switch prod env

--- apps/mobile/src/core/request.ts
+++ apps/mobile/src/core/request.ts
@@ -28,8 +28,8 @@ export const SignApiPlugin: RabbyApiPlugin = {
 export const openapi = new OpenApiService({
   store: {
     // __DEV__ ? 'https://alpha.rabby.io' : 'https://app-api.rabby.io',
-    // host: 'https://app-api.rabby.io',
-    host: 'https://alpha.rabby.io',
+    host: 'https://app-api.rabby.io',
+    // host: 'https://alpha.rabby.io',
   },
   plugin: SignApiPlugin,
   clientName: 'rabbymobile',
