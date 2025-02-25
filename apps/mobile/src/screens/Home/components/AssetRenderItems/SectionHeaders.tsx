d2e032419c4d4e3879d48e4752dfc3cfcdc91156
hz002 <zs4733525@gmail.com>
2025-02-25 11:31:41 +0800

    fix: remove tips

--- apps/mobile/src/screens/Home/components/AssetRenderItems/SectionHeaders.tsx
+++ apps/mobile/src/screens/Home/components/AssetRenderItems/SectionHeaders.tsx
@@ -33,7 +33,7 @@ export const AssestAllHeader = memo(
     const handlePress = (key: AsssetKey) => {
       if (!hasDefi && key === 'defi') {
         setShowDefiTip(true);
-        // delayRemoveTips();
+        delayRemoveTips();
         return;
       }
       if (!hasNft && key === 'nft') {
