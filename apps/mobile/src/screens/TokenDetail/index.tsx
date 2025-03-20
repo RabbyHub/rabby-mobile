3c9eeb35470100cb1f9e8f5ac3e16cdeca626c8e
DMY <147dmy@gmail.com>
2025-03-20 15:16:58 +0800

    revert

--- apps/mobile/src/screens/TokenDetail/index.tsx
+++ apps/mobile/src/screens/TokenDetail/index.tsx
@@ -622,7 +622,7 @@ export const TokenDetailScreen = () => {
               <Button
                 title={t('page.swap.title')}
                 containerStyle={StyleSheet.flatten([styles.btnContainer])}
-                onPress={() => handleSwap('Buy')}
+                onPress={() => handleSwap('Sell')}
                 disabled={!tokenSupportSwap}
               />
             </Tip>
