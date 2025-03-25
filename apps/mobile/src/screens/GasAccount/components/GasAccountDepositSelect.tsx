0dc7fedcd6f1c533a2493c578ce286dcc6ad87a4
Hodor <lost1q84@gmail.com>
2025-03-25 22:15:49 +0800

    fix: gas account current account & pay fee text (#686)

--- apps/mobile/src/screens/GasAccount/components/GasAccountDepositSelect.tsx
+++ apps/mobile/src/screens/GasAccount/components/GasAccountDepositSelect.tsx
@@ -73,7 +73,9 @@ export const GasAccountDepositSelect: React.FC<{
                 )}
               </View>
               <Text style={styles.btnDesc}>
-                {t('page.gasAccount.depositSelectPopup.buyWithDesc')}
+                {Platform.OS === 'ios'
+                  ? t('page.gasAccount.depositPopup.applePayFeeDesc')
+                  : t('page.gasAccount.depositPopup.googlePayFeeDesc')}
               </Text>
             </View>
           }
