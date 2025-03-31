56b33e27502e0a3257b15c086d9232e04910153f
heisenberg <heisenberg-2077@outlook.com>
2025-03-31 18:09:05 +0800

    Update useAutoFocusInput.ts

--- apps/mobile/src/hooks/useAutoFocusInput.ts
+++ apps/mobile/src/hooks/useAutoFocusInput.ts
@@ -1,47 +1,31 @@
-import { useFocusEffect } from '@react-navigation/native';
-import { useCallback, useEffect, useRef, useState } from 'react';
-import { InteractionManager, TextInput } from 'react-native';
-
-const ANIMATED_TRANSITION = 300;
+import { useNavigation } from '@react-navigation/native';
+import { useEffect, useRef, useState } from 'react';
+import { TextInput } from 'react-native';
 
 /**
  * @see https://github.com/react-navigation/react-navigation/issues/11626#issuecomment-1823745730
  */
 export default function useAutoFocusInput(disableAutoFocus = false) {
   const [isInputInitialized, setIsInputInitialized] = useState(false);
-  const [isScreenTransitionEnded, setIsScreenTransitionEnded] = useState(false);
 
   const inputRef = useRef<TextInput>();
-  const focusTimeoutRef = useRef<NodeJS.Timeout>();
+  const navigation = useNavigation();
 
   useEffect(() => {
-    if (
-      !isScreenTransitionEnded ||
-      !isInputInitialized ||
-      !inputRef.current ||
-      disableAutoFocus
-    ) {
+    if (!isInputInitialized || !inputRef.current || disableAutoFocus) {
       return;
     }
-    InteractionManager.runAfterInteractions(() => {
-      inputRef.current?.focus();
-      setIsScreenTransitionEnded(false);
-    });
-  }, [isScreenTransitionEnded, isInputInitialized, disableAutoFocus]);
-
-  useFocusEffect(
-    useCallback(() => {
-      focusTimeoutRef.current = setTimeout(() => {
-        setIsScreenTransitionEnded(true);
-      }, ANIMATED_TRANSITION);
-      return () => {
-        if (!focusTimeoutRef.current) {
-          return;
+    const unsubscribe = navigation.addListener(
+      'transitionEnd' as any,
+      (e: any) => {
+        if (e.data.closing === false) {
+          setTimeout(() => inputRef?.current?.focus());
         }
-        clearTimeout(focusTimeoutRef.current);
-      };
-    }, []),
-  );
+      },
+    );
+
+    return unsubscribe;
+  }, [disableAutoFocus, isInputInitialized, navigation]);
 
   const inputCallbackRef = ref => {
     inputRef.current = ref;
