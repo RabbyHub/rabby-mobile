3b6d8e325a76c4eaa36c1551c85b4db7c1b82146
hz002 <zs4733525@gmail.com>
2025-04-30 10:58:19 +0800

    fix: tips for already in whitelist

--- apps/mobile/src/screens/WhiteList/InputScreen/useAlert.ts
+++ apps/mobile/src/screens/WhiteList/InputScreen/useAlert.ts
@@ -3,6 +3,7 @@ import { isValidHexAddress, Hex } from '@metamask/utils';
 import { useTranslation } from 'react-i18next';
 import { useWhitelist } from '@/hooks/whitelist';
 import { Alert } from 'react-native';
+import { contactService } from '@/core/services';
 
 export const useAlert = (address: string, onConfirm: () => void) => {
   const { t } = useTranslation();
@@ -12,9 +13,13 @@ export const useAlert = (address: string, onConfirm: () => void) => {
   useEffect(() => {
     if (address && isValidHexAddress(address as Hex)) {
       if (isAddrOnWhitelist(address)) {
-        Alert.alert(t('page.whitelist.alreadyInYour'), '', [
-          { text: t('global.ok'), onPress: onConfirm },
-        ]);
+        const aliasName =
+          contactService.getAliasByAddress(address)?.alias || '';
+        Alert.alert(
+          t('page.whitelist.alreadyInYour'),
+          `${address}` + (aliasName ? ` (${aliasName})` : ''),
+          [{ text: t('global.ok'), onPress: onConfirm }],
+        );
       }
     }
   }, [address, isAddrOnWhitelist, onConfirm, t]);
