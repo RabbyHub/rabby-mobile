173d24cd882c7ff111e04b22c31a919543ce5140
heisenberg <heisenberg-2077@outlook.com>
2025-05-06 13:48:47 +0900

    Update useBatchRevoke.ts

--- apps/mobile/src/screens/BatchRevoke/useBatchRevoke.ts
+++ apps/mobile/src/screens/BatchRevoke/useBatchRevoke.ts
@@ -2,15 +2,18 @@ import React from 'react';
 import {
   ApprovalSpenderItemToBeRevoked,
   AssetApprovalSpender,
+  useApprovalsPage,
 } from '../Approvals/useApprovalsPage';
 import { RootNames } from '@/constant/layout';
 import { navigate } from '@/utils/navigation';
 import { findIndexRevokeList } from './utils';
 import { useRevokeOne } from './useRevokeOne';
+import { useApprovalAlertCounts } from '../Home/hooks/approvals';
 
 export const useBatchRevoke = () => {
   const handleRevokeOne = useRevokeOne();
-
+  const { forceUpdate } = useApprovalAlertCounts(10 * 60 * 1000);
+  const { loadApprovals } = useApprovalsPage();
   const handleRevoke = React.useCallback(
     (
       revokeList: ApprovalSpenderItemToBeRevoked[],
@@ -37,7 +40,7 @@ export const useBatchRevoke = () => {
   );
 
   const batchRevoke = React.useCallback(
-    (
+    async (
       revokeList: ApprovalSpenderItemToBeRevoked[],
       dataSource: AssetApprovalSpender[],
     ) => {
@@ -45,11 +48,14 @@ export const useBatchRevoke = () => {
         return;
       }
       if (revokeList.length === 1) {
-        return handleRevokeOne(revokeList[0]);
+        forceUpdate();
+        await handleRevokeOne(revokeList[0]);
+        loadApprovals();
+        return;
       }
       return handleRevoke(revokeList, dataSource);
     },
-    [handleRevoke, handleRevokeOne],
+    [handleRevoke, handleRevokeOne, forceUpdate, loadApprovals],
   );
 
   return batchRevoke;
