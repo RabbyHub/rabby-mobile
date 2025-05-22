655f19d5ecf6b307cbc78ad06ade1dff4f688a0c
vvvvvv1vvvvvv <86296331+vvvvvv1vvvvvv@users.noreply.github.com>
2025-05-22 15:17:39 +0800

    Revert "feat: hide google pay (#819)"
    This reverts commit 71f3f184f4ab06185e38a4b546d007411db01ba0.

--- apps/mobile/src/components/Approval/components/FooterBar/GasLessComponents/GasLessNotEnough.tsx
+++ apps/mobile/src/components/Approval/components/FooterBar/GasLessComponents/GasLessNotEnough.tsx
@@ -6,10 +6,9 @@ import { useTheme2024 } from '@/hooks/theme';
 import { createGetStyles2024 } from '@/utils/styles';
 
 import { ThemeColors2024 } from '@/constant/theme';
-import { Platform, Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { GasAccountCheckResult } from '@rabby-wallet/rabby-api/dist/types';
 import { GasAccountDepositTipPopup } from '@/screens/GasAccount/components/GasAccountDepositTipPopup';
-import { GasAccountDepositWithTokenAlertModal } from '@/screens/GasAccount/components/GasAccountDepositWithTokenAlertModal';
 
 export const GasLessNotEnough: React.FC<{
   gasAccountCost?: GasAccountCheckResult;
@@ -69,33 +68,20 @@ export const GasLessNotEnough: React.FC<{
           </TouchableOpacity>
         ) : null}
       </View>
-      {Platform.OS === 'ios' ? (
-        <GasAccountDepositTipPopup
-          gasAccountAddress={gasAccountAddress}
-          visible={tipPopupVisible}
-          onClose={() => setTipPopupVisible(false)}
-          onDeposit={() => {
-            setTipPopupVisible(false);
-            onDeposit?.();
-          }}
-          onGotoGasAccount={() => {
-            setTipPopupVisible(false);
-            onGotoGasAccount?.();
-          }}
-          minDepositPrice={gasAccountCost?.gas_account_cost?.total_cost}
-        />
-      ) : (
-        <GasAccountDepositWithTokenAlertModal
-          visible={tipPopupVisible}
-          onCancel={() => {
-            setTipPopupVisible(false);
-          }}
-          onConfirm={() => {
-            setTipPopupVisible(false);
-            onGotoGasAccount?.();
-          }}
-        />
-      )}
+      <GasAccountDepositTipPopup
+        gasAccountAddress={gasAccountAddress}
+        visible={tipPopupVisible}
+        onClose={() => setTipPopupVisible(false)}
+        onDeposit={() => {
+          setTipPopupVisible(false);
+          onDeposit?.();
+        }}
+        onGotoGasAccount={() => {
+          setTipPopupVisible(false);
+          onGotoGasAccount?.();
+        }}
+        minDepositPrice={gasAccountCost?.gas_account_cost?.total_cost}
+      />
     </>
   );
 };
