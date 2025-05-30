a3c48f1a694191ec3a84ddfca0193beff2663f30
Hodor <lost1q84@gmail.com>
2025-05-30 18:52:18 +0800

    fix: reject twice (#840)
    * fix: reject twice
    
    * fix: reject

--- apps/mobile/src/components/Approval/components/MiniSignTx/MiniSignTx.tsx
+++ apps/mobile/src/components/Approval/components/MiniSignTx/MiniSignTx.tsx
@@ -1269,7 +1269,10 @@ export const MiniApproval = ({
                 onSubmit={() => {
                   setIsSubmitting(true);
                 }}
-                onReject={handleReject}
+                onReject={e => {
+                  handleReject(e);
+                  dismissedByCodeRef.current = true;
+                }}
                 onResolve={res => {
                   setIsSubmitting(false);
                   onResolve?.(res);
