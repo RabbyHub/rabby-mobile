b2c1b8cbcbdd09ac01b9f14d53c754218d875ebc
hz002 <zs4733525@gmail.com>
2025-06-13 16:33:35 +0800

    fix: lineheight

--- apps/mobile/src/components2024/Button/index.tsx
+++ apps/mobile/src/components2024/Button/index.tsx
@@ -250,7 +250,7 @@ const getStyle = createGetStyles2024(ctx => ({
     paddingVertical: 1,
     fontFamily: 'SF Pro Rounded',
     fontWeight: '700',
-    lineHeight: 24,
+    // lineHeight: 24,
   },
   iconContainer: {
     marginHorizontal: 8,
