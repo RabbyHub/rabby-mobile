0e60dd5ad2cb6dce97389a0637d7b54da005e20c
hz002 <zs4733525@gmail.com>
2025-06-19 21:53:00 +0800

    feat: remove log

--- apps/mobile/src/hooks/useGlobalStatus.ts
+++ apps/mobile/src/hooks/useGlobalStatus.ts
@@ -4,17 +4,9 @@ const PING_URL = 'https://app-api.rabby.io/ping';
 
 async function checkNetwork(): Promise<boolean> {
   try {
-    console.log('CUSTOM_LOGGER:=>: checkNetwork', 'start');
     const resp = await fetch(PING_URL, { method: 'GET' });
-    console.log(
-      'CUSTOM_LOGGER:=>: checkNetwork',
-      'end',
-      Date.now(),
-      resp.status,
-    );
     return resp.status === 200;
   } catch (e) {
-    console.log('CUSTOM_LOGGER:=>: checkNetwork', 'error', e);
     return false;
   }
 }
@@ -54,6 +46,5 @@ networkStatusAtom.onMount = set => {
 
 export const useGlobalStatus = () => {
   const isDisConnnect = useAtomValue(networkStatusAtom);
-  console.log('CUSTOM_LOGGER:=>: isDisconnected', isDisConnnect);
   return { isDisConnnect };
 };
