c45023cfcdc2e762c55c73322a1cdf14fd100426
hz002 <zs4733525@gmail.com>
2025-07-16 11:33:29 +0800

    feat: sort and loading

--- apps/mobile/src/screens/Watchlist/index.tsx
+++ apps/mobile/src/screens/Watchlist/index.tsx
@@ -1,5 +1,11 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { Pressable, ScrollView, Text, View } from 'react-native';
+import {
+  Pressable,
+  ScrollView,
+  Text,
+  View,
+  RefreshControl,
+} from 'react-native';
 
 import { useTheme2024 } from '@/hooks/theme';
 import { createGetStyles2024 } from '@/utils/styles';
@@ -7,12 +13,16 @@ import NormalScreenContainer2024 from '@/components2024/ScreenContainer/NormalSc
 import SearchEntry from './SearchEntry';
 import { EmptyWatchlist } from './EmptyHolder';
 import { TokenListItem } from './TokenItem';
-import TokenHeader, { SortState } from './TokenHeader';
+import TokenHeader from './TokenHeader';
 import { Button } from '@/components2024/Button';
 import { useTranslation } from 'react-i18next';
 import { useFocusEffect } from '@react-navigation/native';
 import { preferenceService } from '@/core/services';
 import { useWatchlistTokens } from './usePinTokens';
+import { navigate } from '@/utils/navigation';
+import { TokenDetailWithPriceCurve } from '@rabby-wallet/rabby-api/dist/types';
+import { RootNames } from '@/constant/layout';
+import { ensureAbstractPortfolioToken } from '../Home/utils/token';
 
 function WatchlistScreen(): JSX.Element {
   const { styles } = useTheme2024({ getStyle });
@@ -24,8 +34,12 @@ function WatchlistScreen(): JSX.Element {
     loading: watchlistLoading,
   } = useWatchlistTokens();
 
-  const [tokenSort, setTokenSort] = useState<SortState>('default');
-  const [changeSort, setChangeSort] = useState<SortState>('default');
+  const [tokenSort, setTokenSort] = useState<'desc' | 'asc' | 'default'>(
+    'default',
+  );
+  const [changeSort, setChangeSort] = useState<'desc' | 'asc' | 'default'>(
+    'default',
+  );
   const [skip, setSkip] = useState(() => preferenceService.getWatchlistSkip());
 
   const showGuide = useMemo(() => {
@@ -61,9 +75,46 @@ function WatchlistScreen(): JSX.Element {
         }
         return (b.price_24h_change ?? 0) - (a.price_24h_change ?? 0);
       }
-      return (b.identity?.fdv ?? 0) - (a.identity?.fdv ?? 0);
+      return (b.credit_score ?? 0) - (a.credit_score ?? 0);
     });
   }, [watchlistTokens, tokenSort, changeSort]);
+
+  const handleOpenTokenDetail = useCallback(
+    (token: TokenDetailWithPriceCurve) => {
+      navigate(RootNames.TokenDetail, {
+        token: ensureAbstractPortfolioToken(token),
+        unHold: false,
+        needUseCacheToken: true,
+      });
+    },
+    [],
+  );
+
+  const handleTokenSort = () => {
+    setTokenSort(prev => {
+      if (prev === 'default') {
+        return 'desc';
+      }
+      if (prev === 'desc') {
+        return 'asc';
+      }
+      return 'default';
+    });
+    setChangeSort('default');
+  };
+
+  const handleChangeSort = () => {
+    setChangeSort(prev => {
+      if (prev === 'default') {
+        return 'desc';
+      }
+      if (prev === 'desc') {
+        return 'asc';
+      }
+      return 'default';
+    });
+    setTokenSort('default');
+  };
 
   return (
     <NormalScreenContainer2024
@@ -76,15 +127,9 @@ function WatchlistScreen(): JSX.Element {
         <>
           <TokenHeader
             tokenSort={tokenSort}
-            onTokenSort={() => {
-              setTokenSort(tokenSort === 'asc' ? 'desc' : 'asc');
-              setChangeSort('default');
-            }}
+            onTokenSort={handleTokenSort}
             changeSort={changeSort}
-            onChangeSort={() => {
-              setChangeSort(changeSort === 'asc' ? 'desc' : 'asc');
-              setTokenSort('default');
-            }}
+            onChangeSort={handleChangeSort}
           />
           <ScrollView style={styles.scrollView}>
             {list.map(item => (
@@ -113,19 +158,24 @@ function WatchlistScreen(): JSX.Element {
             <>
               <TokenHeader
                 tokenSort={tokenSort}
-                onTokenSort={() => {
-                  setTokenSort(tokenSort === 'asc' ? 'desc' : 'asc');
-                  setChangeSort('default');
-                }}
+                onTokenSort={handleTokenSort}
                 changeSort={changeSort}
-                onChangeSort={() => {
-                  setChangeSort(changeSort === 'asc' ? 'desc' : 'asc');
-                  setTokenSort('default');
-                }}
+                onChangeSort={handleChangeSort}
               />
-              <ScrollView style={styles.scrollView}>
+              <ScrollView
+                style={styles.scrollView}
+                refreshControl={
+                  <RefreshControl
+                    refreshing={watchlistLoading}
+                    onRefresh={() => handleFetchTokens(true)}
+                  />
+                }>
                 {list.map(item => (
-                  <TokenListItem key={item.id} item={item} onPress={() => {}} />
+                  <TokenListItem
+                    key={item.id}
+                    item={item}
+                    onPress={handleOpenTokenDetail}
+                  />
                 ))}
               </ScrollView>
             </>
