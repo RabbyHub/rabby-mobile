a8cfc8a8620decae32a4b66ac11a4bd38a3faa30
Hodor <lost1q84@gmail.com>
2025-07-17 15:37:13 +0800

    fix: android webview page provider pending requests (#935)
    * fix: android webview page provider pending requests
    
    * chore: remove console
    
    * chore
    
    * fix: internalMethod account
    
    * feat: update hack rainbowkit
    
    * chore

--- packages/rn-webview-bridge/scripts/inpage-bridge/inpage/metamaskMode.ts
+++ packages/rn-webview-bridge/scripts/inpage-bridge/inpage/metamaskMode.ts
@@ -2,12 +2,18 @@ import { announceProvider } from '@rabby-wallet/universal-providers/dist/EIP6963
 
 import { METAMASK_PROVIDER_INFO } from './constant';
 
-export const setupMetamaskMode = () => {
-  if (!('isRabby' in (window as any).rabby)) {
+let isDone = false;
+export const setupMetamaskMode = (options?: { isKeepRabby?: boolean }) => {
+  if (!('isRabby' in (window as any).rabby) || isDone) {
     return;
   }
-  delete (window as any).rabby.isRabby;
-  delete (window as any).ethereum.isRabby;
+
+  if (!options?.isKeepRabby) {
+    delete (window as any).rabby.isRabby;
+    delete (window as any).ethereum.isRabby;
+  }
+
+  isDone = true;
 
   announceProvider({
     info: METAMASK_PROVIDER_INFO,
