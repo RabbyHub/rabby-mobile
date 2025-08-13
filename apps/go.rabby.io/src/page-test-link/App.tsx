d7967745ebe0efd0e0666fef5de4536200f780b9
richardo2016x <richardo2016x@gmail.com>
2025-08-14 04:05:03 +0800

    feat: tuning.

--- apps/go.rabby.io/src/page-test-link/App.tsx
+++ apps/go.rabby.io/src/page-test-link/App.tsx
@@ -1,20 +1,32 @@
-import { useMemo, useState } from 'react'
+import { useMemo, useState } from 'react';
 
-import './App.less'
+import './App.less';
 
-import rabbyLogo from '/rabby-logo.svg'
+import rabbyLogo from '/rabby-logo.svg';
 
 import { makeRabbySchemeUrl } from '../utils';
 
+const URL_EXAMPLES = [
+  'app.uniswap.org',
+  'debank.com',
+  'superrare.com',
+  'defillama.com',
+  'opensea.io',
+  'zapper.fi',
+  'aave.com',
+  'paras.id',
+  'app.sushi.com',
+];
+
 function App() {
-  const [ targetLink, setTargetLink ] = useState('app.uniswap.org');
+  const [targetLink, setTargetLink] = useState(URL_EXAMPLES[0]);
 
   const rabbySchemeUrls = useMemo(() => {
     return {
       debugUrl: makeRabbySchemeUrl('mobile-debug', targetLink),
       regressionUrl: makeRabbySchemeUrl('mobile-regression', targetLink),
       productionUrl: makeRabbySchemeUrl('mobile-production', targetLink),
-    }
+    };
   }, [targetLink]);
 
   return (
@@ -27,25 +39,52 @@ function App() {
       <h2>Generate Links</h2>
       <div className="card">
         <div>
-          <input className='input' value={targetLink} onChange={(e) => setTargetLink(e.target.value)} placeholder='input target link' />
+          <input
+            className="input"
+            value={targetLink}
+            onChange={e => setTargetLink(e.target.value)}
+            placeholder="input target link"
+          />
+        </div>
+        <div className="url-matrix">
+          {URL_EXAMPLES.map(url_e => {
+            return (
+              <span
+                key={`u-${url_e}`}
+                className="url-example"
+                onClick={() => setTargetLink(url_e)}
+              >
+                {url_e}
+              </span>
+            );
+          })}
         </div>
 
-        <div className='divider' />
+        <div className="divider" />
 
-        <p>
-          <a target='_blank' href={rabbySchemeUrls.debugUrl} className='anchor-button open-rabby'>
-            Open Rabby(Debug)
+        <div className='openbtn-matrix'>
+          <a
+            target="_blank"
+            href={rabbySchemeUrls.regressionUrl}
+            className="anchor-button open-rabby">
+            Regression
           </a>
-          <a target='_blank' href={rabbySchemeUrls.regressionUrl} className='anchor-button open-rabby'>
-            Open Rabby(Regression)
+          <a
+            target="_blank"
+            href={rabbySchemeUrls.productionUrl}
+            className="anchor-button open-rabby">
+            Production
           </a>
-          <a target='_blank' href={rabbySchemeUrls.productionUrl} className='anchor-button open-rabby'>
-            Open Rabby(Production)
+          <a
+            target="_blank"
+            href={rabbySchemeUrls.debugUrl}
+            className="anchor-button open-rabby">
+            Debug
           </a>
-        </p>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
