4e2ca22130b2d0018079674e676a8a2441c57035
richardo2016x <richardo2016x@gmail.com>
2025-08-14 21:28:47 +0800

    feat: use safe json replacer for rabby poster injected to webview.

--- packages/rn-webview-bridge/src/browserScripts.ts
+++ packages/rn-webview-bridge/src/browserScripts.ts
@@ -1,14 +1,38 @@
+const posterName = `__rabbyPostMessageToProvider${Math.random().toString(36).substr(2, 9)}`;
+const posterRef = `window['${posterName}']`;
+/**
+ * @tip for WeakSet, reference https://caniuse.com/?search=WeakSet
+ * @tip safeJsonStringifyReplacer trim these:
+ *  - circular references
+ *  - __react, maybe from rsc hydration
+ *  - BigInt, convert to string with 'n' suffix
+ */
 export const BROWSER_SCRIPT_BASE = `
 ;(function () {
-  if (!window.__rabbyPostMessageToProvider) {
-    window.__rabbyPostMessageToProvider = function (content, pos) {
-      if (typeof content === 'object') {
-        content = { primitive: content };
-      }
+  if (!${posterRef}) {
+    var safeJsonStringifyReplacer = (function () {
+      var cache = new WeakSet();
+      return function (key, value) {
+        if (key.indexOf('__reactFiber') === 0) return 'TRIMED';
+        if (typeof value === 'object' && value !== null) {
+          if (cache.has(value)) return;
+          cache.add(value);
+        }
+        if (typeof value === 'bigint') {
+          return value.toString() + 'n';
+        }
+        return value;
+      };
+    })();
+
+    ${posterRef} = function (content, pos) {
+      if (typeof content !== 'object') content = { primitive: content };
+
       pos = pos || 'unknown';
       var jsonString = "";
+
       try {
-        jsonString = JSON.stringify(content)
+        jsonString = JSON.stringify(content, safeJsonStringifyReplacer)
       } catch (e) {
         jsonString = JSON.stringify({
           type: 'BROWSER_SCRIPT_ERR_CAPTURED',
@@ -33,21 +57,25 @@ export const SPA_urlChangeListener = `;(function () {
   var __rabbyPushState = __rabbyHistory.pushState;
   var __rabbyReplaceState = __rabbyHistory.replaceState;
   function __rabby__updateUrl() {
-    var siteName = document.querySelector('head > meta[property="og:site_name"]');
-    var title = siteName || document.querySelector('head > meta[name="title"]') || document.title;
-    var height = Math.max(document.documentElement.clientHeight, document.documentElement.scrollHeight, document.body.clientHeight, document.body.scrollHeight);
+    var siteNameMeta = document.querySelector('head > meta[property="og:site_name"]');
+    var siteName = siteNameMeta ? siteNameMeta.getAttribute('content') : '';
+    var title = (function () {
+      var titleMeta = document.querySelector('head > meta[name="title"]');
+      return titleMeta ? titleMeta.getAttribute('content') : '';
+    })() || document.title;
 
-    window.__rabbyPostMessageToProvider({
+    ${posterRef}({
       type: 'NAV_CHANGE',
       payload: {
         url: location.href,
         title: title,
+        ogSiteName: siteName,
       }
     }, 'NAV_CHANGE');
 
     setTimeout(() => {
       var height = Math.max(document.documentElement.clientHeight, document.documentElement.scrollHeight, document.body.clientHeight, document.body.scrollHeight);
-      window.__rabbyPostMessageToProvider({
+      ${posterRef}({
         type: 'GET_HEIGHT',
         payload: {
           height: height
@@ -83,7 +111,7 @@ export const JS_WINDOW_INFORMATION = `
 
     var siteName = document.querySelector('head > meta[property="og:site_name"]');
     var title = siteName || document.querySelector('head > meta[name="title"]');
-    window.__rabbyPostMessageToProvider({
+    ${posterRef}({
       type: 'GET_TITLE_FOR_BOOKMARK',
       payload: {
         title: title ? title.content : document.title,
