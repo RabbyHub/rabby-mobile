d3190ffe00001ba67c81c2eab613056cf86b5c30
Hodor <lost1q84@gmail.com>
2025-09-04 15:36:52 +0800

    fix: style

--- apps/mobile/src/screens/Perps/index.tsx
+++ apps/mobile/src/screens/Perps/index.tsx
@@ -127,10 +127,12 @@ export const PerpsScreen = () => {
             marketDataMap={marketDataMap}
           />
           <PerpsMarketSection marketData={marketData} />
-          <PerpsHistorySection
-            marketDataMap={marketDataMap}
-            historyList={homeHistoryList}
-          />
+          {isLogin ? (
+            <PerpsHistorySection
+              marketDataMap={marketDataMap}
+              historyList={homeHistoryList}
+            />
+          ) : null}
           <PerpsIntro />
         </ScrollView>
       </NormalScreenContainer2024>
