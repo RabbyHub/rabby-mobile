25bf4a6d6fa0fe9de215bcca6b6e0e63477ec402
DMY <147dmy@gmail.com>
2025-09-09 14:49:03 +0800

    feat: update Trezor dependencies and assets

--- apps/mobile/src/core/keyring-bridge/trezor/trezor-bridge.ts
+++ apps/mobile/src/core/keyring-bridge/trezor/trezor-bridge.ts
@@ -5,43 +5,24 @@ import { Linking } from 'react-native';
 
 export default class TrezorBridge implements TrezorBridgeInterface {
   isDeviceConnected = false;
-  // event = eventBus;
   model = '';
   connectDevices = new Set<string>();
   init: TrezorBridgeInterface['init'] = async config => {
-    // TrezorConnect.on('DEVICE_EVENT', (event: any) => {
-    //   if (event && event.payload && event.payload.features) {
-    //     this.model = event.payload.features.model;
-    //   }
-    //   const currentDeviceId = event.payload?.id;
-    //   if (event.type === 'device-connect') {
-    //     this.connectDevices.add(currentDeviceId);
-
-    //     // this.event.emit('cleanUp', true);
-    //   }
-    //   if (event.type === 'device-disconnect') {
-    //     this.connectDevices.delete(currentDeviceId);
-    //     // this.event.emit('cleanUp', true);
-    //   }
-    //   console.debug('trezor DEVICE_EVENT', event.type, event);
-    // });
-
     if (!this.isDeviceConnected) {
       TrezorConnect.init({
         debug: appIsDev,
         manifest: {
-          email: 'developer@xyz.com',
-          appName: 'Trezor Connect Example',
-          appUrl: 'https://your.application.com',
+          email: 'support@rabby.io',
+          appName: 'Rabby Wallet',
+          appUrl: 'https://rabby.io/',
         },
         // for local development purposes. for production, leave it undefined to use the default value.
-        // connectSrc: 'https://connect.trezor.io/9/', //isEmulator ? 'trezorsuitelite://connect' : undefined,
-        connectSrc: appIsDev
-          ? 'trezorsuitelite://connect'
-          : 'https://connect.trezor.io/9/',
+        // connectSrc: appIsDev
+        //   ? 'trezorsuitelite://connect'
+        //   : 'https://connect.trezor.io/9/',
+        connectSrc: 'https://connect.trezor.io/9/',
 
-        deeplinkOpen: url => {
-          // eslint-disable-next-line no-console
+        deeplinkOpen: async url => {
           console.debug('deeplinkOpen', url);
           Linking.openURL(url);
         },
