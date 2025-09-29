4041ced89515a991984bb1f9d790271514c7d000
Hodor <lost1q84@gmail.com>
2025-09-29 17:04:20 +0800

    feat: add rate limit & fix sync networks

--- apps/mobile/src/core/storage/serviceStoreStub.ts
+++ apps/mobile/src/core/storage/serviceStoreStub.ts
@@ -23,7 +23,6 @@ import {
   currencyService,
   dappService,
 } from '../services/shared';
-import { apisSafe } from '../apis/safe';
 
 /**
  * @description only call this hook on app's top level
@@ -112,9 +111,5 @@ export function useSetupServiceStub() {
 
   useMount(() => {
     initEventBus();
-  });
-
-  useMount(() => {
-    apisSafe.syncAllGnosisNetworks();
   });
 }
