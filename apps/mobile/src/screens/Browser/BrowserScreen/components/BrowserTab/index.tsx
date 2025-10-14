617211e4d2d236ae42812e67099262caee2858fd
Hodor <lost1q84@gmail.com>
2025-10-14 10:34:27 +0800

    revert

--- apps/mobile/src/screens/Browser/BrowserScreen/components/BrowserTab/index.tsx
+++ apps/mobile/src/screens/Browser/BrowserScreen/components/BrowserTab/index.tsx
@@ -670,7 +670,7 @@ export const BrowserTab = React.forwardRef<BrowserRef, BrowserTabProps>(
                         // });
                         return webviewActions.onNavigationStateChange(event);
                       }}
-                      onOpenWindow={handleOnOpenWindow}
+                      // onOpenWindow={handleOnOpenWindow}
                       webviewDebuggingEnabled={isNonPublicProductionEnv}
                       contentMode={contentMode}
                       {...(contentMode === 'desktop' && {
