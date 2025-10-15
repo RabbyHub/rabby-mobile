d03caeb5b90f75461fe9b8a42f6fc14e481b4895
Hodor <lost1q84@gmail.com>
2025-10-15 18:04:51 +0800

    feat: change press area

--- apps/mobile/src/screens/Browser/BottomSheetBrowser/BrowserHandler.tsx
+++ apps/mobile/src/screens/Browser/BottomSheetBrowser/BrowserHandler.tsx
@@ -188,27 +188,33 @@ export const BrowserHandler = () => {
           <View style={styles.handleComponentContainer}>
             {activeTabState.url ? (
               <DropdownMenuView menuConfig={menuConfigs}>
+                <View style={styles.handleItem}>
+                  <RcIconMore1CC
+                    width={24}
+                    height={24}
+                    color={colors2024['neutral-title-1']}
+                  />
+                </View>
+              </DropdownMenuView>
+            ) : (
+              <View style={styles.handleItem}>
                 <RcIconMore1CC
                   width={24}
                   height={24}
                   color={colors2024['neutral-title-1']}
                 />
-              </DropdownMenuView>
-            ) : (
-              <RcIconMore1CC
-                width={24}
-                height={24}
-                color={colors2024['neutral-title-1']}
-              />
+              </View>
             )}
             <View style={styles.divider} />
 
             <TouchableOpacity onPress={handleCloseBrowser} hitSlop={5}>
-              <RcIconClose1CC
-                width={24}
-                height={24}
-                color={colors2024['neutral-title-1']}
-              />
+              <View style={styles.handleItemLast}>
+                <RcIconClose1CC
+                  width={24}
+                  height={24}
+                  color={colors2024['neutral-title-1']}
+                />
+              </View>
             </TouchableOpacity>
           </View>
         </View>
@@ -236,13 +242,19 @@ const getStyle = createGetStyles2024(({ colors2024, isLight }) => {
         : colors2024['neutral-bg-5'],
       alignItems: 'center',
       flexDirection: 'row',
-      paddingVertical: 4,
-      paddingHorizontal: 12,
       borderRadius: 19,
       gap: 8,
 
       borderWidth: StyleSheet.hairlineWidth,
       borderColor: isLight ? colors2024['neutral-InvertHighlight'] : '#000',
+    },
+    handleItem: {
+      paddingVertical: 4,
+      paddingLeft: 12,
+    },
+    handleItemLast: {
+      paddingVertical: 4,
+      paddingRight: 12,
     },
     divider: {
       // width: StyleSheet.hairlineWidth,
