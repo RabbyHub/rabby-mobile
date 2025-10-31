2a2d71c6f5649a430706583e8b3a0f774b4a90b2
hz002 <zs4733525@gmail.com>
2025-10-31 14:22:44 +0800

    fix: visible

--- apps/mobile/src/components/AccountSwitcher/hooks.ts
+++ apps/mobile/src/components/AccountSwitcher/hooks.ts
@@ -38,6 +38,7 @@ const DefaultStates: {
 
   History: makeDefaultState(),
   MultiHistory: makeDefaultState(),
+  Lending: makeDefaultState(),
 
   // Receive: makeDefaultState(),
   // GasAccount: makeDefaultState(),
