ef79adedd3b89560052c9cac6873176720370ba2
richardo2016x <richardo2016x@gmail.com>
2025-11-13 19:39:35 +0800

    build: go.rabby.io

--- apps/go.rabby.io/src/utils.ts
+++ apps/go.rabby.io/src/utils.ts
@@ -24,16 +24,29 @@ export function makeRabbySchemeUrl(rabbyGoEnv: string, targetLink: string) {
   if (!targetLink) return '';
 
   targetLink = stringUtils.ensurePrefix(targetLink, 'https://');
-  const ret = { pathPrefix: '' };
+  const ret = { domain: '', pathPrefix: '' };
 
   switch (rabbyGoEnv) {
     default:
-    case 'mobile-debug': { ret.pathPrefix = 'mobile-debug'; break; }
-    case 'mobile-regression': { ret.pathPrefix = 'mobile-regression'; break; }
-    case 'mobile-production': { ret.pathPrefix = 'mobile'; break; }
+    case 'mobile-debug': {
+      ret.domain = 'go-debug.rabby.io';
+      ret.pathPrefix = 'mobile-debug';
+      break;
+    }
+    case 'mobile-regression': {
+      ret.domain = 'go-regression.rabby.io';
+      ret.pathPrefix = 'mobile-regression';
+      break;
+    }
+    case 'mobile-production': {
+      ret.domain = 'go-production.rabby.io';
+      ret.pathPrefix = 'mobile';
+      break;
+    }
   }
 
-  const openLink = `go.rabby.io/${ret.pathPrefix}/index.html?_cmd=open-dapp&dapp=${encodeURIComponent(targetLink)}`;
+  // const openLink = `${ret.domain}/${ret.pathPrefix}/index.html?_cmd=open-dapp&dapp=${encodeURIComponent(targetLink)}`;
+  const openLink = `${ret.domain}/${ret.pathPrefix}/?_cmd=open-dapp&dapp=${encodeURIComponent(targetLink)}`;
   return formatRabbySchemeUrl(rabbyGoEnv, stringUtils.ensurePrefix(openLink, 'https://'));
 }
 
