2c504fa728ff6c412faf3a571094ff0439be9716
kim <180150277+jinming0618@users.noreply.github.com>
2025-12-04 10:25:57 +0800

    fix: revert test code

--- apps/mobile/src/screens/PerpsMarketDetail/index.tsx
+++ apps/mobile/src/screens/PerpsMarketDetail/index.tsx
@@ -87,7 +87,7 @@ export const PerpsMarketDetailScreen = () => {
     marketDataMap,
     perpFee,
     marketData,
-    // hasPermission,
+    hasPermission,
     currentPerpsAccount,
     isLogin,
     userFills,
@@ -97,7 +97,7 @@ export const PerpsMarketDetailScreen = () => {
 
     handleDeleteAgent,
   } = usePerpsState();
-  const hasPermission = true;
+  // const hasPermission = true;
   const [isShowModal, setIsShowModal] = useState(false);
   const [amountVisible, setAmountVisible] = useState(false);
   const [selectedToken, setSelectedToken] = useSelectedToken();
