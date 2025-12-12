80eb794691cc1d164a3d0740983cd6cfb9e32a15
vvvvvv1vvvvvv <86296331+vvvvvv1vvvvvv@users.noreply.github.com>
2025-12-12 19:38:28 +0800

    feat: rm favorite in assets page (#1301)
    * feat: rm favorite in assets page
    
    * fix: wrap with try catch
    
    * fix: rm favorite in single address page

--- apps/mobile/src/screens/Home/TokenList.tsx
+++ apps/mobile/src/screens/Home/TokenList.tsx
@@ -1,10 +1,4 @@
-import React, {
-  useCallback,
-  useState,
-  useMemo,
-  useEffect,
-  useRef,
-} from 'react';
+import React, { useCallback, useState, useMemo, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { RefreshControl } from 'react-native-gesture-handler';
 
@@ -52,10 +46,6 @@ export const icons = {
   unfoldLight: require('@/assets/icons/ios_ic_rabby_icons/ic_rabby_menu_unfold.png'),
   foldDark: require('@/assets/icons/ios_ic_rabby_icons/ic_rabby_menu_fold_dark.png'),
   foldLight: require('@/assets/icons/ios_ic_rabby_icons/ic_rabby_menu_fold.png'),
-  pinDark: require('@/assets/icons/ios_ic_rabby_icons/ic_rabby_menu_token_favorite_dark.png'),
-  pinLight: require('@/assets/icons/ios_ic_rabby_icons/ic_rabby_menu_token_favorite.png'),
-  unpinDark: require('@/assets/icons/ios_ic_rabby_icons/ic_rabby_menu_token_unfavorite_dark.png'),
-  unpinLight: require('@/assets/icons/ios_ic_rabby_icons/ic_rabby_menu_token_unfavorite.png'),
 };
 
 interface Props {
@@ -274,37 +264,6 @@ export const TokenList = ({
                 chainId: data.chain,
               });
               toast.success(t('page.tokenDetail.actionsTips.fold_success'));
-            }
-            singleTokenRefresh();
-            tokenRefresh();
-          },
-        },
-        {
-          title: data._isPined
-            ? t('page.tokenDetail.action.unfavorite')
-            : t('page.tokenDetail.action.favorite'),
-          icon: data._isPined
-            ? isLight
-              ? icons.unpinLight
-              : icons.unpinDark
-            : isLight
-            ? icons.pinLight
-            : icons.pinDark,
-          androidIconName: data._isPined
-            ? 'ic_rabby_menu_token_unfavorite'
-            : 'ic_rabby_menu_token_favorite',
-          key: 'favorite',
-          action() {
-            if (data._isPined) {
-              preferenceService.removePinedToken({
-                tokenId: data._tokenId,
-                chainId: data.chain,
-              });
-            } else {
-              preferenceService.pinToken({
-                tokenId: data._tokenId,
-                chainId: data.chain,
-              });
             }
             singleTokenRefresh();
             tokenRefresh();
