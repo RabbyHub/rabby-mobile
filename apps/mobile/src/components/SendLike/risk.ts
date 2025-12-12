0730a7bc0c6701035ccb83b9c0c185ca78b0fd4a
richardo2016x <richardo2016x@gmail.com>
2025-12-12 23:30:36 +0800

    feat: support forbidden check tip

--- apps/mobile/src/components/SendLike/risk.ts
+++ apps/mobile/src/components/SendLike/risk.ts
@@ -1,5 +1,4 @@
 import { openapi } from '@/core/request';
-import { useMyAccounts } from '@/hooks/account';
 import {
   AddrDescResponse,
   ProjectItem,
@@ -10,7 +9,7 @@ import PQueue from 'p-queue';
 import { isSameAddress } from '@rabby-wallet/base-utils/dist/isomorphic/address';
 import { getAddrDescWithCexLocalCacheSync } from '@/databases/hooks/cex';
 import { getSortedAddressList, sortAccountList } from '@/core/apis/account';
-import { preferenceService } from '@/core/services';
+import { useDebouncedValue } from '@/hooks/common/delayLikeValue';
 
 const queue = new PQueue({ intervalCap: 5, concurrency: 5, interval: 1000 });
 
@@ -24,20 +23,62 @@ const waitQueueFinished = (q: PQueue) => {
   });
 };
 
+const reqForbiddenTip = async (forbiddenCheck: ForBiddenCheckParams) => {
+  const allValuesSet =
+    forbiddenCheck?.chain_id &&
+    forbiddenCheck?.to_addr &&
+    forbiddenCheck?.user_addr &&
+    forbiddenCheck?.id;
+  if (allValuesSet) {
+    return await openapi
+      .checkTokenDepositForbidden({
+        chain_id: forbiddenCheck?.chain_id || 'eth',
+        to_addr: forbiddenCheck?.to_addr || '',
+        user_addr: forbiddenCheck?.user_addr || '',
+        id: forbiddenCheck?.id || '',
+      })
+      .then(res => {
+        return res?.msg || '';
+      })
+      .catch(error => {
+        console.error('checkTokenDepositForbidden error', error);
+        return null;
+      });
+  } else {
+    return null;
+  }
+};
+
+type ForBiddenCheckParams = {
+  user_addr: string;
+  id?: string;
+  chain_id?: string;
+  to_addr: string;
+};
+
 export const enum RiskType {
   NEVER_SEND = 1,
   SCAM_ADDRESS = 2,
   CONTRACT_ADDRESS = 3,
   CEX_NO_DEPOSIT = 4,
+
+  FORBIDDEN_TIP = 5,
 }
 type RiskItem = { type: RiskType; value: string };
 export const useRisks = (options: {
   toAddress: string;
   fromAddress?: string;
+  forbiddenCheck?: ForBiddenCheckParams;
   cex?: ProjectItem | null;
   onLoadFinished?: (ctx: { risks: Array<RiskItem> }) => void;
 }) => {
-  const { fromAddress, toAddress, cex, onLoadFinished } = options;
+  const {
+    fromAddress,
+    toAddress,
+    forbiddenCheck: input_forbiddenCheck,
+    cex,
+    onLoadFinished,
+  } = options;
   const [risks, setRisks] = useState<Array<RiskItem>>([]);
   const { t } = useTranslation();
   const [loading, setLoading] = useState(!!toAddress);
@@ -50,6 +91,48 @@ export const useRisks = (options: {
   const hasSend = useMemo(() => {
     return !loading && !risks.some(r => r.type === RiskType.NEVER_SEND);
   }, [loading, risks]);
+
+  const memoForbiddenCheck = useMemo(() => {
+    return {
+      chain_id: input_forbiddenCheck?.chain_id,
+      id: input_forbiddenCheck?.id,
+      user_addr: input_forbiddenCheck?.user_addr || '',
+      to_addr: input_forbiddenCheck?.to_addr || '',
+    };
+  }, [
+    input_forbiddenCheck?.chain_id,
+    input_forbiddenCheck?.id,
+    input_forbiddenCheck?.user_addr,
+    input_forbiddenCheck?.to_addr,
+  ]);
+
+  const forbiddenCheck = useDebouncedValue(memoForbiddenCheck, 300);
+
+  const fetchForbiddenCheck = useCallback(async () => {
+    reqForbiddenTip(forbiddenCheck).then(tip => {
+      if (!tip) {
+        setRisks(prev => {
+          const filtered = prev.filter(r => r.type !== RiskType.FORBIDDEN_TIP);
+          return filtered;
+        });
+      } else {
+        setRisks(prev => {
+          const filtered = prev.filter(r => r.type !== RiskType.FORBIDDEN_TIP);
+          return [
+            ...filtered,
+            {
+              type: RiskType.FORBIDDEN_TIP,
+              value: tip,
+            },
+          ];
+        });
+      }
+    });
+  }, [forbiddenCheck]);
+
+  useEffect(() => {
+    fetchForbiddenCheck();
+  }, [fetchForbiddenCheck]);
 
   const fetchRisks = useCallback(async () => {
     if (!toAddress) return;
@@ -196,10 +279,11 @@ export const useRisks = (options: {
 };
 
 const riskTypePriority = {
-  [RiskType.CEX_NO_DEPOSIT]: 1,
-  [RiskType.NEVER_SEND]: 11,
-  [RiskType.CONTRACT_ADDRESS]: 111,
-  [RiskType.SCAM_ADDRESS]: 1111,
+  [RiskType.CEX_NO_DEPOSIT]: 10e-1,
+  [RiskType.NEVER_SEND]: 10,
+  [RiskType.CONTRACT_ADDRESS]: 10e1,
+  [RiskType.SCAM_ADDRESS]: 10e3,
+  [RiskType.FORBIDDEN_TIP]: 10e4,
 };
 
 export function sortRisksDesc(a: { type: RiskType }, b: { type: RiskType }) {
