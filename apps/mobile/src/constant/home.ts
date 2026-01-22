61ce68b4561711fdd199aa6817225962ce51dc23
richardo2016x <104543757+richardo2016x@users.noreply.github.com>
2026-01-22 18:43:46 +0800

    feat: improve ux on home drawer (#1379)
    * init commit
    
    * feat: swipe
    
    * fix: android swipe
    
    * chore
    
    * fix: height
    
    * wip
    
    * wip: react native guesture
    
    * feat: use reanimated & react native gesture
    
    * feat: use panResponder
    
    * fix: android
    
    * feat: home dapp drawer
    
    * chore
    
    * chore
    
    * feat: improve ux on home drawer
    
    * chore: code cleanup
    
    * fix: tuning on iOS.
    
    * feat: also apply to Android
    
    * feat: align android ux with ios
    
    * feat: tuning gesture
    
    * style: tuning
    
    * style: tuning
    
    * fix: tuning
    
    * style: tuning
    
    * chore: cleanup
    
    ---------
    
    Co-authored-by: vvvvvv1vvvvvv <galennnnnnn@gmail.com>
    Co-authored-by: Hodor <lost1q84@gmail.com>

--- apps/mobile/src/constant/home.ts
+++ apps/mobile/src/constant/home.ts
@@ -1,3 +1,10 @@
 export const HOME_REFRESH_INTERVAL = 10 * 60 * 1000;
 export const ITEM_LAYOUT_PADDING_HORIZONTAL = 16;
 export const ITEM_GRID_GAP = 10;
+
+export const HOME_TOP_HEADER_SIZES = {
+  headerHeight: 52,
+  headerPaddingY: 14,
+  tabItemHeight: 54,
+  portfolioContainerPx: 16,
+};
