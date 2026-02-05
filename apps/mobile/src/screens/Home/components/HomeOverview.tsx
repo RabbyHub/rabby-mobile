ce0d0e040d590a89b3d368e5144e6de81f31abd8
Hodor <lost1q84@gmail.com>
2026-02-05 14:34:31 +0800

    fix: home swipe hint style

--- apps/mobile/src/screens/Home/components/HomeOverview.tsx
+++ apps/mobile/src/screens/Home/components/HomeOverview.tsx
@@ -368,6 +368,7 @@ const getStyle = createGetStyles2024(
     main: {
       height: '100%',
       overflow: 'hidden',
+
       // flex: 1,
       // ...makeDevOnlyStyle({
       //   backgroundColor: colors2024['red-light-2'],
@@ -484,7 +485,7 @@ const getStyle = createGetStyles2024(
       flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
-      marginTop: 16,
+      marginTop: 36,
       // ...makeDebugBorder('purple'),
     },
     swipeUpHintText: {
