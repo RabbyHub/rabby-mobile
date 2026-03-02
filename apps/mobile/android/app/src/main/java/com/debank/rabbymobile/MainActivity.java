353c5647641db176f786a050ee5a5d4a474393fe
Jiàn <hi@chabuduo.in>
2026-03-02 18:01:01 +0800

    feat: android build

--- apps/mobile/android/app/src/main/java/com/debank/rabbymobile/MainActivity.java
+++ apps/mobile/android/app/src/main/java/com/debank/rabbymobile/MainActivity.java
@@ -1,22 +1,20 @@
 package com.debank.rabbymobile;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
-import android.os.Bundle;
-import org.devio.rn.splashscreen.SplashScreen; // react-native-splash-screen
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    // SplashScreen.show(this);
-    // https://github.com/crazycodeboy/react-native-splash-screen/blob/b47197626804a742b8569cad50d5e0ed92fc765c/android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java#L25
     SplashScreen.show(this, R.style.SplashScreenTheme, true);
 
-    // super.onCreate(savedInstanceState);
-    // fix: https://sentry.io/organizations/debank/issues/?groupStatsPeriod=24h&page=0&project=6312337&query=is%3Aunresolved&referrer=issue-list&statsPeriod=14d
     // https://github.com/software-mansion/react-native-screens#android
     super.onCreate(null);
   }
@@ -32,12 +30,12 @@ public class MainActivity extends ReactActivity {
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
-   * (aka React 18) with two boolean flags.
+   * DefaultReactActivityDelegate} which allows you to easily enable New Architecture with a single
+   * boolean flag {@link fabricEnabled}.
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
+    return new DefaultReactActivityDelegate(this, getMainComponentName(), DefaultNewArchitectureEntryPoint.getFabricEnabled()) {
       @Override
       protected Bundle getLaunchOptions() {
         Bundle initialProperties = new Bundle();
