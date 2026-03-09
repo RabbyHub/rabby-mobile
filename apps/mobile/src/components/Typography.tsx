20086633845a2b24923e13cf8d49eed99edbc380
Jiàn <hi@chabuduo.in>
2026-03-09 14:50:43 +0800

    fix: migrate defaultProps to wrapper components

--- apps/mobile/src/components/Typography.tsx
+++ apps/mobile/src/components/Typography.tsx
@@ -1,10 +1,4 @@
 /* eslint-disable no-restricted-imports */
-// This file is the central wrapper for Text components.
-// Currently re-exports original components as a smooth transition.
-// After React 19 migration, we will implement withDefaults wrapper.
-
-// Future implementation for React 19 (when defaultProps is removed):
-/*
 import React from 'react';
 import { Text as RNText, TextInput as RNTextInput } from 'react-native';
 import {
@@ -27,16 +21,12 @@ function withDefaults<P extends object>(
 export const Text = withDefaults(RNText, { allowFontScaling: false });
 export const TextInput = withDefaults(RNTextInput, { allowFontScaling: false });
 export const RNGHText = withDefaults(RNGHTextImpl, { allowFontScaling: false });
-export const RNGHTextInput = withDefaults(RNGHTextInputImpl, { allowFontScaling: false });
-export const AnimateableText = withDefaults(AnimateableTextImpl, { allowFontScaling: false });
-export const RNEUIText = withDefaults(RNEUITextImpl, { allowFontScaling: false });
-*/
-
-// Current implementation - direct re-exports:
-export { Text, TextInput } from 'react-native';
-export {
-  Text as RNGHText,
-  TextInput as RNGHTextInput,
-} from 'react-native-gesture-handler';
-export { default as AnimateableText } from 'react-native-animateable-text';
-export { Text as RNEUIText } from '@rneui/base';
+export const RNGHTextInput = withDefaults(RNGHTextInputImpl, {
+  allowFontScaling: false,
+});
+export const AnimateableText = withDefaults(AnimateableTextImpl, {
+  allowFontScaling: false,
+});
+export const RNEUIText = withDefaults(RNEUITextImpl, {
+  allowFontScaling: false,
+});
--- apps/mobile/src/core/utils/fonts.ts
+++ apps/mobile/src/core/utils/fonts.ts
@@ -1,43 +1,7 @@
-/* eslint-disable no-restricted-imports */
-import { Text as RNText, TextInput as RNTextInput } from 'react-native';
-import {
-  Text as RNGHText,
-  TextInput as RNGHTextInput,
-} from 'react-native-gesture-handler';
-
 import type { StyleProp, TextStyle } from 'react-native';
-import AnimateableText from 'react-native-animateable-text';
 
 import { bizNumberUtils } from '@rabby-wallet/biz-utils';
-import { IS_ANDROID, IS_IOS } from '../native/utils';
-import { Text as RNEUIText } from '@rneui/base';
-
-if (IS_ANDROID || IS_IOS) {
-  // @ts-expect-error
-  RNText.defaultProps = Object.assign({}, RNText.defaultProps, {
-    allowFontScaling: false,
-  });
-  // @ts-expect-error
-  RNTextInput.defaultProps = Object.assign({}, RNTextInput.defaultProps, {
-    allowFontScaling: false,
-  });
-  RNGHText.defaultProps = Object.assign({}, RNGHText.defaultProps, {
-    allowFontScaling: false,
-  });
-  RNGHTextInput.defaultProps = Object.assign({}, RNGHTextInput.defaultProps, {
-    allowFontScaling: false,
-  });
-  AnimateableText.defaultProps = Object.assign(
-    {},
-    AnimateableText.defaultProps,
-    {
-      allowFontScaling: false,
-    },
-  );
-  RNEUIText.defaultProps = Object.assign({}, RNEUIText.defaultProps, {
-    allowFontScaling: false,
-  });
-}
+import { IS_IOS } from '../native/utils';
 
 export const enum FontWeightEnum {
   thin = 100,
