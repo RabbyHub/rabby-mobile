c347e07c6c7661627d34ff78b6ec780a08b0720d
richardo2016x <richardo2016x@gmail.com>
2026-05-22 16:23:14 +0800

    fix: show synced extension accounts immediately

--- packages/service-keyring/src/keyringService.ts
+++ packages/service-keyring/src/keyringService.ts
@@ -1605,6 +1605,14 @@ export class KeyringService extends RNEventEmitter {
 
     await this._updateMemStoreKeyrings();
 
+    addedAccounts.forEach(account => {
+      this.emit('newAccount', {
+        address: account.address,
+        brandName: account.brandName || account.type,
+        type: (account.type || account.brandName) as KeyringTypeName,
+      });
+    });
+
     return addedAccounts;
   }
 
