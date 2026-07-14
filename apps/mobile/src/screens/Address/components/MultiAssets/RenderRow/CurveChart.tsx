39ea4a1cffb983a4533166f7995f1b1d669abeb7
richardo2016x <richardo2016x@gmail.com>
2026-07-14 11:31:43 +0800

    perf: unmount hidden balance skeleton

--- apps/mobile/src/screens/Address/components/MultiAssets/RenderRow/CurveChart.tsx
+++ apps/mobile/src/screens/Address/components/MultiAssets/RenderRow/CurveChart.tsx
@@ -387,41 +387,37 @@ const ChartHeader = React.memo(
     return (
       <Animated.View style={styles.charHeader}>
         <View style={styles.netWorthContainer}>
-          <Pressable
-            style={[
-              styles.netWorthTextContainer,
-              showNetWorthLoading ? styles.hidden : undefined,
-            ]}
-            onPress={onPressNetWorth}
-            {...makeTestIDProps(E2E_ID.home.portfolioBalanceValue)}>
-            <RefreshNudgedTickerText
-              value={formatNetWorth}
-              animateWidth={false}
-              maxLength={24}
-              lineHeight={42}
-              duration={320}
-              style={[
-                styles.netWorth,
-                hideType === 'HALF_HIDE' ? styles.balanceOpacity : null,
-              ]}
-              fontSizeByLength={{
-                maxFontSize: MAX_NETWORTH_FS,
-                minFontSize: MIN_NETWORTH_FS,
-                threshold: NETWORTH_FIT_LEN,
-              }}
+          {showNetWorthLoading ? (
+            <Skeleton
+              {...makeTestIDProps(E2E_ID.home.portfolioBalanceLoading)}
+              width={181}
+              height={42}
+              style={styles.skeletonNetWorth}
+              LinearGradientComponent={LoadingLinear}
             />
-          </Pressable>
-
-          <Skeleton
-            {...makeTestIDProps(E2E_ID.home.portfolioBalanceLoading)}
-            width={181}
-            height={42}
-            style={[
-              styles.skeletonNetWorth,
-              !showNetWorthLoading && styles.hidden,
-            ]}
-            LinearGradientComponent={LoadingLinear}
-          />
+          ) : (
+            <Pressable
+              style={styles.netWorthTextContainer}
+              onPress={onPressNetWorth}
+              {...makeTestIDProps(E2E_ID.home.portfolioBalanceValue)}>
+              <RefreshNudgedTickerText
+                value={formatNetWorth}
+                animateWidth={false}
+                maxLength={24}
+                lineHeight={42}
+                duration={320}
+                style={[
+                  styles.netWorth,
+                  hideType === 'HALF_HIDE' ? styles.balanceOpacity : null,
+                ]}
+                fontSizeByLength={{
+                  maxFontSize: MAX_NETWORTH_FS,
+                  minFontSize: MIN_NETWORTH_FS,
+                  threshold: NETWORTH_FIT_LEN,
+                }}
+              />
+            </Pressable>
+          )}
 
           <Pressable
             style={({ pressed }) => [
