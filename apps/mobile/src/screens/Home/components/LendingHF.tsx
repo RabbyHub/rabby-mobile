4eb2eadc6c005185d79156bf7b7be4b4178c3ceb
hz002 <zs4733525@gmail.com>
2026-07-17 13:38:19 +0800

    style: hide lending overview when global hidden

--- apps/mobile/src/screens/Home/components/LendingHF.tsx
+++ apps/mobile/src/screens/Home/components/LendingHF.tsx
@@ -7,18 +7,28 @@ import { formatUsdValue } from '@/utils/number';
 import { createGetStyles2024 } from '@/utils/styles';
 import { useEffect } from 'react';
 import { Text } from '@/components/Typography';
+import { BALANCE_HIDE_TYPE, useHideBalance } from '../hooks/useHideBalance';
 
-const NetWorthBadge: React.FC<{ netWorth: string }> = ({ netWorth }) => {
+const NetWorthBadge: React.FC<{ netWorth: string; isHidden: boolean }> = ({
+  netWorth,
+  isHidden,
+}) => {
   const { styles } = useTheme2024({ getStyle: getStyles });
   if (Number(netWorth) <= 0) {
     return null;
   }
-  return <Text style={styles.netWorthText}>{formatUsdValue(netWorth)}</Text>;
+  return (
+    <Text style={styles.netWorthText}>
+      {isHidden ? '****' : formatUsdValue(netWorth)}
+    </Text>
+  );
 };
 
 export const LendingHF: React.FC<{}> = () => {
   const { styles } = useTheme2024({ getStyle: getStyles });
   const { lendingHf } = useLendingHF();
+  const [hideType] = useHideBalance();
+  const isHidden = hideType === BALANCE_HIDE_TYPE.HIDE;
 
   useRendererDetect({ name: 'LendingHF' });
 
@@ -39,7 +49,15 @@ export const LendingHF: React.FC<{}> = () => {
     Number(lendingHf.healthFactor) <= 0 ||
     Number(lendingHf.healthFactor) >= 3
   ) {
-    return <NetWorthBadge netWorth={lendingHf?.netWorthUSD || '0'} />;
+    return (
+      <NetWorthBadge
+        netWorth={lendingHf?.netWorthUSD || '0'}
+        isHidden={isHidden}
+      />
+    );
+  }
+  if (isHidden) {
+    return <Text style={styles.netWorthText}>****</Text>;
   }
   return (
     <Text
