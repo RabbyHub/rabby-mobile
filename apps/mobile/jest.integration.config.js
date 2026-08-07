52f7e1f2246e38b3e23fe167b14bb48b587d25ba
richardo2016x <richardo2016x@gmail.com>
2026-08-07 22:16:07 +0800

    test: cover authentication readiness integration

--- apps/mobile/jest.integration.config.js
+++ apps/mobile/jest.integration.config.js
@@ -3,6 +3,20 @@ const baseConfig = require('./jest.config');
 module.exports = {
   ...baseConfig,
   displayName: 'mobile-integration',
+  setupFiles: [...baseConfig.setupFiles, '<rootDir>/jest.integration.setup.js'],
+  moduleNameMapper: {
+    ...baseConfig.moduleNameMapper,
+    '^react$': '<rootDir>/node_modules/react',
+    '^react-native$': '<rootDir>/node_modules/react-native',
+    '^@rneui/base$': '<rootDir>/jest.integration.rneui.js',
+    '^@rneui/themed$': '<rootDir>/jest.integration.rneui.js',
+    '\\.svg$': '<rootDir>/jest.integration.svg.js',
+    '^@rabby-wallet/biz-utils/dist/(.*)$':
+      '<rootDir>/../../packages/biz-utils/src/$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@rneui|react-native-reanimated|react-native-animateable-text|react-native-root-toast|react-native-root-siblings|p-queue|p-timeout|eventemitter3)/)',
+  ],
   testMatch: ['<rootDir>/src/**/*.integration.test.[jt]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/'],
 };
