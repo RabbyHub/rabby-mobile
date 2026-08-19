4901c8e00bdfd2a0dbba04f95ed7a3755156d029
Hodor <lost1q84@gmail.com>
2026-08-19 11:58:32 +0800

    chore

--- apps/mobile/src/screens/Approvals/index.tsx
+++ apps/mobile/src/screens/Approvals/index.tsx
@@ -32,7 +32,6 @@ import { CustomMaterialTabBar } from '@/components2024/CustomTabs/CustomMaterial
 import type { Account } from '@/core/startupServices/preference';
 import { useRoute } from '@react-navigation/native';
 import type { GetNestedScreenRouteProp } from '@/navigation-type';
-import { Text } from '@/components/Typography';
 const isAndroid = Platform.OS === 'android';
 
 const ApprovalScreenContainer: React.FC<{
