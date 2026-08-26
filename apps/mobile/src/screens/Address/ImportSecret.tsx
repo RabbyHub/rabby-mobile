c2aaec3789c9646c928b4a71ebd9c49eb8d94afc
DMY <147dmy@gmail.com>
2026-08-26 09:32:12 +0800

    fix: clear import error after scanning

--- apps/mobile/src/screens/Address/ImportSecret.tsx
+++ apps/mobile/src/screens/Address/ImportSecret.tsx
@@ -395,14 +395,10 @@ export const ImportSecret = ({ route }: ScreenProps) => {
   // Handle scanner result
   React.useEffect(() => {
     if (scanner.text) {
-      if (activeTab === 'seedPhrase') {
-        setMnemonics(scanner.text);
-      } else {
-        setPrivateKey(scanner.text);
-      }
+      handleInputChange(activeTab, scanner.text);
       scanner.clear();
     }
-  }, [scanner, activeTab]);
+  }, [scanner, activeTab, handleInputChange]);
 
   const isConfirmDisabled = React.useMemo(() => {
     if (activeTab === 'seedPhrase') {
